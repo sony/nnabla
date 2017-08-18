156e352de881cd0a833bd78340d09e906300610d
Takuya Narihira <Takuya.Narihira@sony.com>
2017-08-18 17:17:49 +0900

    [c++][example] Remove logger from mnist_runtime

--- examples/cpp/mnist_runtime/mnist_runtime.cpp
+++ examples/cpp/mnist_runtime/mnist_runtime.cpp
@@ -12,9 +12,9 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include <nbla/logger.hpp>
 #include <nbla_utils/nnp.hpp>
 
+#include <cassert>
 #include <fstream>
 #include <iomanip>
 #include <iostream>
