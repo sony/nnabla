58711864ea5cad8c7315062b5be17d36329dc7dd
Stephen Tiedemann <stephen.tiedemann@gmail.com>
2017-09-01 11:58:58 +0200

    removed proprietary license notice

--- include/nbla/function/embed.hpp
+++ include/nbla/function/embed.hpp
@@ -12,15 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// -*- coding:utf-8 -*-
-/*
- * Copyright (C) 2016 Sony Corporation
- * This is UNPUBLISHED PROPRIETARY SOURCE CODE of Sony Corporation;
- * the contents of this file is not to be disclosed to third parties, copied
- * or duplicated in any form, in whole or in part, without the prior written
- * permission of Sony Corporation.
- */
-
 /** Embed
  */
 #ifndef __NBLA_FUNCTION_EMBED_HPP__
