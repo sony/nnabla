03075bcd8fdec81f69ef887a4be5e45cd7235898
Yoshiyuki Kobayashi <Yoshiyuki.Kobayashi@jp.sony.com>
2017-09-24 10:53:13 +0900

    Fix

--- python/src/nnabla/utils/cli/forward.py
+++ python/src/nnabla/utils/cli/forward.py
@@ -92,7 +92,7 @@ def update_result(args, index, result, values, output_index, type_end_names):
                         # CSV type
                         with open(full_path, 'w') as f:
                             writer = csv.writer(f, lineterminator='\n')
-                            x = np.array(d, dtype=np.float32).transpose()
+                            x = np.array(d, dtype=np.float32)
                             writer.writerows(x)
                     outputs[data_index].append(os.path.join('.', file_name))
         output_index += 1
