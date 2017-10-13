4cf9cfaa3ab7c77bf45f3e36079af7bc348515e8
Yukio.Obuchi <yukio.obuchi@jp.sony.com>
2017-10-13 17:02:06 +0900

    Support threading

--- python/src/nnabla/utils/data_iterator.py
+++ python/src/nnabla/utils/data_iterator.py
@@ -22,6 +22,7 @@ Detailed design document is :doc:`/doc/designs/data_iterator`.
 import atexit
 import numpy
 import six
+import threading
 
 from .data_source import DataSourceWithFileCache
 from .data_source import DataSourceWithMemoryCache
@@ -84,6 +85,10 @@ class DataIterator(object):
         self._size = data_source.size
 
         self._reset()
+        self._current_data = None
+        self._next_thread = threading.Thread(target=self._next)
+        self._next_thread.start()
+
         self._closed = False
         atexit.register(self.close)
 
@@ -171,6 +176,15 @@ class DataIterator(object):
 
         self._data_source.reset()
 
+    def _next(self):
+        data = [[] for x in self._variables]
+        batch_size = self._batch_size
+        for b in range(batch_size):
+            d = self._get_next_data()
+            for i, v in enumerate(self._variables):
+                data[i].append(d[i])
+        self._current_data = tuple([numpy.array(x) for x in data])
+
     def next(self):
         '''next
 
@@ -183,13 +197,11 @@ class DataIterator(object):
         Returns:
             tuple: tuple of data for mini-batch in numpy.ndarray.
         '''
-        data = [[] for x in self._variables]
-        batch_size = self._batch_size
-        for b in range(batch_size):
-            d = self._get_next_data()
-            for i, v in enumerate(self._variables):
-                data[i].append(d[i])
-        return tuple([numpy.array(x) for x in data])
+        self._next_thread.join()
+        data = self._current_data
+        self._next_thread = threading.Thread(target=self._next)
+        self._next_thread.start()
+        return data
 
     def _callback_epoch_end(self):
         for callback in self._epoch_end_callbacks:
