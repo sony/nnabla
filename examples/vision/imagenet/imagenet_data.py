d70051ee725cad99f526801cec60ba25b4cced1a
Hideho.Gomi <Hideho.Gomi@sony.com>
2017-10-16 14:01:56 +0900

    add file for imagenet.

---
+++ examples/vision/imagenet/imagenet_data.py
@@ -0,0 +1,18 @@
+# Copyright (c) 2017 Sony Corporation. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from nnabla.utils.data_iterator import data_iterator_cache
+
+def data_iterator_imagenet(batch_size, cache_dir):
+    return data_iterator_cache(cache_dir, batch_size, shuffle=True, normalize=False)
