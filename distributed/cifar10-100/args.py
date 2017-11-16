6a0f78894acc7815cc9b7a5f362c42bce0d7ec99
KazukiYoshiyama <Kazuki.Yoshiyama@jp.sony.com>
2017-11-16 22:19:49 +0900

    Add distributed training over multi-gpu and multi-node

---
+++ distributed/cifar10-100/args.py
@@ -0,0 +1,47 @@
+# Copyright (c) 2017 Sony Corporation. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+def get_args(monitor_path='tmp.monitor', max_iter=40000, model_save_path='tmp.monitor', learning_rate=1e-3, batch_size=64, weight_decay=0, n_devices=4, warmup_epoch=5):
+    """
+    Get command line arguments.
+
+    Arguments set the default values of command line arguments.
+    """
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--batch-size", "-b", type=int, default=batch_size)
+    parser.add_argument("--learning-rate", "-l",
+                        type=float, default=learning_rate)
+    parser.add_argument("--monitor-path", "-m",
+                        type=str, default=monitor_path)
+    parser.add_argument("--max-iter", "-i", type=int, default=max_iter)
+    parser.add_argument("--val-interval", "-v", type=int, default=100)
+    parser.add_argument("--val-iter", "-j", type=int, default=100)
+    parser.add_argument("--weight-decay", "-w",
+                        type=float, default=weight_decay)
+    parser.add_argument("--device-id", "-d", type=int, default=0)
+    parser.add_argument("--n-devices", "-n", type=int, default=n_devices)
+    parser.add_argument("--warmup-epoch", "-e", type=int, default=warmup_epoch)
+    parser.add_argument("--model-save-interval", "-s", type=int, default=1000)
+    parser.add_argument("--model-save-path", "-o",
+                        type=str, default=model_save_path)
+    parser.add_argument('--context', '-c', type=str,
+                        default=None, help="Extension path. ex) cpu, cuda.cudnn.")
+    parser.add_argument("--net", type=str,
+                        default='cifar10_resnet23',
+                        help="Neural network architecure type (used only in classification.py).\n"
+                        "'cifar10_resnet23'\n"
+                        "'cifar100_resnet23'")
+    return parser.parse_args()
---
+++ distributed/cifar10-100/models.py
@@ -0,0 +1,99 @@
+# Copyright (c) 2017 Sony Corporation. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+import os
+import time
+from args import get_args
+from cifar10_data import data_iterator_cifar10
+from cifar100_data import data_iterator_cifar100
+import nnabla as nn
+import nnabla.communicators as C
+from nnabla.contrib.context import extension_context
+import nnabla.functions as F
+import nnabla.parametric_functions as PF
+import nnabla.solvers as S
+import numpy as np
+
+
+def categorical_error(pred, label):
+    # TODO: Use F.top_n_error
+    """
+    Compute categorical error given score vectors and labels as
+    numpy.ndarray.
+    """
+    pred_label = pred.argmax(1)
+    return (pred_label != label.flat).mean()
+
+
+def resnet23_prediction(image, test=False, rng=None, ncls=10, nmaps=64, act=F.relu):
+    """
+    Construct ResNet 23
+    """
+    # Residual Unit
+    def res_unit(x, scope_name, rng, dn=False, test=False):
+        C = x.shape[1]
+        with nn.parameter_scope(scope_name):
+            # Conv -> BN -> Nonlinear
+            with nn.parameter_scope("conv1"):
+                h = PF.convolution(x, C / 2, kernel=(1, 1), pad=(0, 0),
+                                   with_bias=False)
+                h = PF.batch_normalization(h, batch_stat=not test)
+                h = act(h)
+            # Conv -> BN -> Nonlinear
+            with nn.parameter_scope("conv2"):
+                h = PF.convolution(h, C / 2, kernel=(3, 3), pad=(1, 1),
+                                   with_bias=False)
+                h = PF.batch_normalization(h, batch_stat=not test)
+                h = act(h)
+            # Conv -> BN
+            with nn.parameter_scope("conv3"):
+                h = PF.convolution(h, C, kernel=(1, 1), pad=(0, 0),
+                                   with_bias=False)
+                h = PF.batch_normalization(h, batch_stat=not test)
+            # Residual -> Nonlinear
+            h = act(F.add2(h, x, inplace=True))
+            # Maxpooling
+            if dn:
+                h = F.max_pooling(h, kernel=(2, 2), stride=(2, 2))
+            return h
+    # Conv -> BN -> Nonlinear
+    with nn.parameter_scope("conv1"):
+        # Preprocess
+        if not test:
+            image = F.image_augmentation(image, contrast=1.0,
+                                         angle=0.25,
+                                         flip_lr=True)
+            image.need_grad = False
+        h = PF.convolution(image, nmaps, kernel=(3, 3),
+                           pad=(1, 1), with_bias=False)
+        h = PF.batch_normalization(h, batch_stat=not test)
+        h = act(h)
+
+    h = res_unit(h, "conv2", rng, False)    # -> 32x32
+    h = res_unit(h, "conv3", rng, True)     # -> 16x16
+    h = res_unit(h, "conv4", rng, False)    # -> 16x16
+    h = res_unit(h, "conv5", rng, True)     # -> 8x8
+    h = res_unit(h, "conv6", rng, False)    # -> 8x8
+    h = res_unit(h, "conv7", rng, True)     # -> 4x4
+    h = res_unit(h, "conv8", rng, False)    # -> 4x4
+    h = F.average_pooling(h, kernel=(4, 4))  # -> 1x1
+    pred = PF.affine(h, ncls, rng=rng)
+
+    return pred
+
+
+def loss_function(pred, label):
+    loss = F.mean(F.softmax_cross_entropy(pred, label))
+    return loss
