c33fc832e04520fc6e68ffd83809f04e5f11f6dc
Takuya Narihira <Takuya.Narihira@sony.com>
2018-01-03 12:14:33 +0900

    [c++] init.hpp general interfaces

--- include/nbla/init.hpp
+++ include/nbla/init.hpp
@@ -30,5 +30,19 @@ NBLA_API void init_cpu();
    Clear CPU memory cache.
  */
 NBLA_API void clear_cpu_memory_cache();
+
+/** Get CPU array classes.
+*/
+NBLA_API vector<string> cpu_array_classes();
+
+/** Set CPU array classes
+*/
+NBLA_API void _cpu_set_array_classes(const vector<string> &a);
+
+NBLA_API void cpu_device_synchronize(const string &device);
+
+NBLA_API int cpu_get_device_count();
+
+NBLA_API vector<string> cpu_get_devices();
 }
 #endif
