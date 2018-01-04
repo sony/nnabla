6ae056f3ac5f23b9011fd54e4c7a167b63874e61
Takuya Narihira <Takuya.Narihira@sony.com>
2018-01-04 12:49:44 +0900

    [generate] init.cpp

--- build-tools/code_generator/generate2.py
+++ build-tools/code_generator/generate2.py
@@ -19,12 +19,30 @@ from os.path import abspath, dirname, join
 from utils.load_function_rst import Functions
 from utils.common import check_update
 from utils.type_conv import type_from_proto
-import itertools
 
 here = abspath(dirname(abspath(__file__)))
 base = abspath(join(here, '../..'))
 
 import code_generator_utils as utils
+
+
+def info_to_list(info):
+    '''Returns a list of (name, snake_name, [argument types as c++ type])'''
+    items = []
+    for name, item in info.items():
+        items.append((name, item['snake_name'], [
+            type_from_proto[v['type']]['cpp'] for v in item.get('arguments', {}).values()]))
+    return items
+
+
+def generate_init(function_info, function_types, solver_info, solver_types):
+    # Create function list
+    function_list = info_to_list(function_info)
+    # Create solver list
+    solver_list = info_to_list(solver_info)
+    utils.generate_from_template(
+        join(base, 'src/nbla/init.cpp.tmpl'),
+        **utils.dict_filter(locals(), ['function_list', 'function_types', 'solver_list', 'solver_types']))
 
 
 def generate_solver_python_intereface(solver_info):
@@ -34,7 +52,18 @@ def generate_solver_python_intereface(solver_info):
         join(base, 'python/src/nnabla/solver.pxd.tmpl'), solver_info=solver_info)
 
 
-def generate():
+def generate_function_types_template(function_info):
+    from itertools import chain
+    list(chain(*[cat.values for cat in function_info['Functions'].values()]))
+
+
+def generate(function_info):
     solver_info = utils.load_yaml_ordered(
         open(join(here, 'solvers.yaml'), 'r'))
+    function_info = utils.load_function_info(flatten=True)
+    function_types = utils.load_yaml_ordered(open(
+        join(here, 'function_types.yaml'), 'r'))
+    solver_types = utils.load_yaml_ordered(open(
+        join(here, 'solver_types.yaml'), 'r'))
+    generate_init(function_info, function_types, solver_info, solver_types)
     generate_solver_python_intereface(solver_info)
