23f0ebc6ac66aa1b98670ad9b701719eb85c5d95
Masato Hori <masato@fixstars.com>
2018-01-18 16:04:11 +0900

    make installしなくてもいいようにクラスをまるごと単体テストへ移動

--- python/test/utils/test_conversion.py
+++ python/test/utils/test_conversion.py
@@ -15,14 +15,93 @@
 import os
 import nnabla.utils.converter
 import onnx
+from nnabla.utils import nnabla_pb2
+from onnx import (defs, checker, helper, numpy_helper, mapping,
+                  ModelProto, GraphProto, NodeProto, AttributeProto, TensorProto, OperatorSetIdProto)
 import onnx_caffe2.backend
 import numpy as np
 import nnabla as nn
-from nnabla.utils.converter.onnx import OnnxReader, OnnxExporter
+#from nnabla.utils.converter.onnx import OnnxReader, OnnxExporter
 
 TEST_DATA_DIR="conversion_data"
 #def test_conversion():
 #    nnabla.utils.converter.convert_files(args, args.files, output)
+
+def onnx_model_to_protobuf(onnx_model):
+    protobuf = nnabla_pb2.NNablaProtoBuf()
+    # convert onnx model to nnabla protobuf
+    #for ifile in self._args:
+    #    print('Reading {}'.format(ifile))
+    #    ext = os.path.splitext(ifile)[1].lower()
+    #    if ext == '.nnp':
+    #        try:
+    #            tmpdir = tempfile.mkdtemp()
+    #            with zipfile.ZipFile(ifile, 'r') as nnpzip:
+    #                for name in nnpzip.namelist():
+    #                    if os.path.splitext(name)[1].lower() in ['.nntxt', '.prototxt']:
+    #                        nnpzip.extract(name, tmpdir)
+    #                        with open(os.path.join(tmpdir, name), 'rt') as f:
+    #                            text_format.Merge(f.read(), self._nnp)
+    #                for name in nnpzip.namelist():  # Param
+    #                    if os.path.splitext(name)[1].lower() in ['.protobuf', '.h5']:
+    #                        nnpzip.extract(name, tmpdir)
+    #                        self.load_parameters(
+    #                            os.path.join(tmpdir, name))
+    #        finally:
+    #            shutil.rmtree(tmpdir)
+    #    elif ext in ['.nntxt', '.prototxt']:
+    #        with open(ifile, 'rt') as f:
+    #            text_format.Merge(f.read(), self._nnp)
+    #    elif ext in ['.protobuf', '.h5']:
+    #        self.load_parameters(ifile)
+
+    #if self._expand_network:
+    #    self._nnp = expander.NnpExpander(self._nnp).expand()
+
+    class nnp:
+        pass
+    nnp.protobuf = protobuf
+    return nnp
+
+class OnnxReader:
+    def __init__(self, file_path):
+        self._file_path = file_path
+
+    #def load_parameters(self, filename):
+    #    e = os.path.splitext(filename)[1].lower()
+    #    if e == '.h5':
+    #        import h5py
+    #        with h5py.File(filename, 'r') as hd:
+    #            keys = []
+
+    #            def _get_keys(name):
+    #                ds = hd[name]
+    #                if not isinstance(ds, h5py.Dataset):
+    #                    # Group
+    #                    return
+    #                # To preserve order of parameters
+    #                keys.append((ds.attrs.get('index', None), name))
+    #            hd.visit(_get_keys)
+    #            for _, key in sorted(keys):
+    #                ds = hd[key]
+    #                parameter = self._nnp.parameter.add()
+    #                parameter.variable_name = key
+    #                parameter.shape.dim.extend(ds.shape)
+    #                parameter.data.extend(ds[...].flatten())
+    #                if ds.attrs['need_grad']:
+    #                    parameter.need_grad = True
+    #                else:
+    #                    parameter.need_grad = False
+
+    #    elif e == '.protobuf':
+    #        with open(filename, 'rb') as f:
+    #            self._nnp.MergeFromString(f.read())
+
+    def read(self):
+        model_proto = ModelProto()
+        with open(self._file_path, "rb") as f:
+            model_proto.ParseFromString(f.read())
+        return onnx_model_to_protobuf(model_proto)
 
 def test_onnx_nnp_conversion_relu():
     path = os.path.join(TEST_DATA_DIR, "relu.onnx")
