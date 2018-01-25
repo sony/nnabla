0365b84869c179ba0b5b4dfacf3b52b690f7b9b3
Yukio Oobuchi <Yukio.Oobuchi@sony.com>
2018-01-25 18:06:54 +0900

    Change expand algo.

--- python/src/nnabla/utils/network.py
+++ python/src/nnabla/utils/network.py
@@ -75,14 +75,14 @@ class Network:
 
     def forward_function(self, func):
         try:
-            ## Uncomment when debugging expand_recurrent
+            # Uncomment when debugging expand_recurrent
             # print(func.name)
             # print(func.function_instance)
             # for n, inp in enumerate(func.variable_inputs):
             #     print('   IN:', n, inp.shape, inp.d.flatten()[0])
             func.function_instance.forward(
                 func.variable_inputs, func.variable_outputs)
-            ## Uncomment when debugging expand_recurrent
+            # Uncomment when debugging expand_recurrent
             # for n, out in enumerate(func.variable_outputs):
             #     print('  OUT:', n, out.shape, out.d.flatten()[0])
         except:
