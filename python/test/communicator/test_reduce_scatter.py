d5f925f4997f0d306ea49eba3860309df20fd4d7
Takuya Narihira <Takuya.Narihira@sony.com>
2018-03-23 11:43:42 +0900

    [test] Simplify communicator tests

--- python/test/communicator/test_reduce_scatter.py
+++ python/test/communicator/test_reduce_scatter.py
@@ -17,30 +17,9 @@ import nnabla as nn
 import nnabla.parametric_functions as PF
 import nnabla.communicators as C
 import numpy as np
-from nbla_test_utils import list_context
 from nnabla.contrib.context import extension_context
 
-############################################
-# Communicator has to be instantiated here,
-# otherwise, mpirun fails.
-############################################
-
-# Communicator
-comm = None
-try:
-    extension_module = "cuda"
-    ctx = extension_context(extension_module)
-    comm = C.MultiProcessDataParalellCommunicator(ctx)
-    comm.init()
-    n_devices = comm.size
-    mpi_rank = comm.rank
-    mpi_local_rank = comm.local_rank
-    device_id = mpi_local_rank
-    ctx.device_id = str(device_id)
-except:
-    pass
-
-############################################
+from six.moves import reduce
 
 
 def ref_reduce_scatter(x_data_list, size, division):
@@ -54,29 +33,22 @@ def ref_reduce_scatter(x_data_list, size, division):
     return results
 
 
-@pytest.mark.skipif(comm == None, reason="Communicator does not exist.")
 @pytest.mark.parametrize("seed", [313])
 @pytest.mark.parametrize("division", [True, False])
-def test_reduce_scatter(seed, division):
-    try:
-        import nnabla_ext
-        import nnabla_ext.cuda
-    except:
-        pytest.skip("{} is supported in CUDA device".format(
-            sys._getframe().f_code.co_name))
-
-    n_devices = nnabla_ext.cuda.init.get_device_count()
-    if n_devices < 2:
-        pytest.skip("Number of cuda devices in this machine is less than 2.")
-
-    if n_devices != comm.size:
-        pytest.skip("Number of cuda devices is not same as that of processes.")
+def test_reduce_scatter(seed, division, comm_nccl_opts):
+    if comm_nccl_opts is None:
+        pytest.skip(
+            "Communicator test is disabled. You can turn it on by an option `--test-communicator`.")
+    comm = comm_nccl_opts.comm
+    device_id = int(comm_nccl_opts.device_id)
+    devices = comm_nccl_opts.devices
+    n_devices = len(comm_nccl_opts.devices)
 
     # Variables
     rng = np.random.RandomState(seed)
     x_list = []
     x_data_list = []
-    for i in range(n_devices):
+    for i in devices:
         x_data = rng.rand(3, 4)
         x_data_list.append(x_data)
         x = nn.Variable(x_data.shape)
@@ -85,7 +57,8 @@ def test_reduce_scatter(seed, division):
 
     # Reduce Scatter
     x = nn.Variable((3, 4))
-    comm.reduce_scatter([x.data for x in x_list], x.data, division=division)
+    comm.reduce_scatter(
+        [x.data for x in x_list], x.data, division=division)
 
     # Ref
     refs = ref_reduce_scatter(x_data_list, n_devices, division)
