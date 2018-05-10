8750ef80f5c3a72fd2fc14302372b6d97253a6b0
KazukiYoshiyama <Kazuki.Yoshiyama@jp.sony.com>
2018-05-10 21:45:35 +0900

    clip_grad_by_norm.

---
+++ include/nbla/function/clip_grad_by_norm.hpp
@@ -0,0 +1,84 @@
+// Copyright (c) 2017 Sony Corporation. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef __NBLA_FUNCTION_CLIPGRADBYNORM_HPP__
+#define __NBLA_FUNCTION_CLIPGRADBYNORM_HPP__
+
+#include <nbla/cpu.hpp>
+#include <nbla/function.hpp>
+#include <nbla/function_registry.hpp>
+
+namespace nbla {
+
+NBLA_REGISTER_FUNCTION_HEADER(ClipGradByNorm, float, const vector<int> &);
+
+/** ClipGradByNorm
+@f[
+g_x = clip\_norm \times \frac{g_y}{\|g_y\|_2}
+@f]
+
+Inputs:
+- N-D array.
+
+Outputs:
+- N-D array.
+
+@tparam T Data type for computation.
+@param clip_norm Value of the scalar to clip to the norm of input to `clip_norm`
+in the forward pass.
+@param axes Axes to be reduced. If empty list is given, all dimensions are
+reduced to scalar. This is used in the forward pass.
+
+\ingroup FunctionImplGrp
+ */
+template <typename T>
+class ClipGradByNorm : public BaseFunction<float, const vector<int> &> {
+
+protected:
+  float clip_norm_;
+  const vector<int> axes_;
+
+  shared_ptr<Function> sum_;
+  shared_ptr<Function> pow_scalar_;
+  shared_ptr<Function> broadcast_;
+
+public:
+  ClipGradByNorm(const Context &ctx, float clip_norm, const vector<int> &axes)
+      : BaseFunction<float, const vector<int> &>(ctx, clip_norm, axes),
+        clip_norm_(clip_norm), axes_(axes) {}
+  virtual ~ClipGradByNorm() {}
+  virtual shared_ptr<Function> copy() const {
+    return create_ClipGradByNorm(ctx_, clip_norm_, axes_);
+  }
+  virtual vector<dtypes> in_types() { return vector<dtypes>{get_dtype<T>()}; }
+  virtual vector<dtypes> out_types() { return vector<dtypes>{get_dtype<T>()}; }
+  virtual int min_inputs() { return 1; }
+  virtual int min_outputs() { return 1; }
+  virtual string name() { return "ClipGradByNorm"; }
+  virtual vector<string> allowed_array_classes() {
+    return SingletonManager::get<Cpu>()->array_classes();
+  }
+
+protected:
+  NBLA_API virtual void setup_impl(const Variables &inputs,
+                                   const Variables &outputs);
+  NBLA_API virtual void forward_impl(const Variables &inputs,
+                                     const Variables &outputs);
+  NBLA_API virtual void backward_impl(const Variables &inputs,
+                                      const Variables &outputs,
+                                      const vector<bool> &propagate_down,
+                                      const vector<bool> &accum);
+};
+}
+#endif
---
+++ src/nbla/function/generic/clip_grad_by_norm.cpp
@@ -0,0 +1,115 @@
+// Copyright (c) 2017 Sony Corporation. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+/** ClipGradByNorm
+ */
+#include <nbla/array.hpp>
+#include <nbla/function/clip_grad_by_norm.hpp>
+#include <nbla/variable.hpp>
+
+#include <nbla/function/broadcast.hpp>
+#include <nbla/function/pow_scalar.hpp>
+#include <nbla/function/sum.hpp>
+
+#include <algorithm>
+
+namespace nbla {
+
+NBLA_REGISTER_FUNCTION_SOURCE(ClipGradByNorm, float, const vector<int> &);
+
+template <typename T>
+void ClipGradByNorm<T>::setup_impl(const Variables &inputs,
+                                   const Variables &outputs) {
+  outputs[0]->reshape(inputs[0]->shape(), true);
+  sum_ = create_Sum(this->ctx_, axes_, true);
+  pow_scalar_ = create_PowScalar(this->ctx_, 2.);
+
+  vector<int> _shape;
+  for (auto v : inputs[0]->shape()) {
+    _shape.push_back(v);
+  }
+  broadcast_ = create_Broadcast(this->ctx_, _shape);
+}
+
+template <typename T>
+void ClipGradByNorm<T>::forward_impl(const Variables &inputs,
+                                     const Variables &outputs) {
+  const T *x = inputs[0]->get_data_pointer<T>(this->ctx_);
+  T *y = outputs[0]->cast_data_and_get_pointer<T>(this->ctx_, true);
+  for (int i = 0; i < inputs[0]->size(); i++) {
+    y[i] = x[i];
+  }
+}
+
+template <typename T, bool accum>
+void clip_grad_by_norm_backward_cpu(int size, T clip_norm_grad, T *dx,
+                                    const T *dy, const T *m) {
+  for (int s = 0; s < size; ++s) {
+    T _dx = clip_norm_grad * dy[s] / std::sqrt(m[s]);
+    accum ? dx[s] += _dx : dx[s] = _dx;
+  }
+}
+
+template <typename T> void clip_grad_by_norm_copy(int size, T *m, const T *dy) {
+  for (int s = 0; s < size; ++s) {
+    m[s] = dy[s];
+  }
+}
+
+template <typename T>
+void ClipGradByNorm<T>::backward_impl(const Variables &inputs,
+                                      const Variables &outputs,
+                                      const vector<bool> &propagate_down,
+                                      const vector<bool> &accum) {
+  if (!propagate_down[0]) {
+    return;
+  }
+
+  auto shape = inputs[0]->shape();
+  Variable v0(shape);
+  Variable v1(shape);
+  Variable v2(shape);
+  Variable v3(shape);
+  auto intermediates0 = Variables{&v0};
+  auto intermediates1 = Variables{&v1};
+  auto intermediates2 = Variables{&v2};
+  auto intermediates3 = Variables{&v3};
+
+  Size_t size = inputs[0]->size();
+  T *_m = intermediates0[0]->cast_data_and_get_pointer<T>(this->ctx_, true);
+  const T *_dy = outputs[0]->get_grad_pointer<T>(this->ctx_);
+  clip_grad_by_norm_copy(size, _m, _dy);
+
+  // power grads by 2.
+  pow_scalar_->setup(intermediates0, intermediates1);
+  pow_scalar_->forward(intermediates0, intermediates1);
+
+  // sum grads powered by 2.
+  sum_->setup(intermediates1, intermediates2);
+  sum_->forward(intermediates1, intermediates2);
+
+  // broadcast
+  broadcast_->setup(intermediates2, intermediates3);
+  broadcast_->forward(intermediates2, intermediates3);
+
+  T *dx = inputs[0]->cast_grad_and_get_pointer<T>(this->ctx_, !accum[0]);
+  const T *dy = outputs[0]->get_grad_pointer<T>(this->ctx_);
+  const T *m = intermediates3[0]->get_data_pointer<T>(this->ctx_);
+  if (accum[0])
+    clip_grad_by_norm_backward_cpu<T, true>(size, clip_norm_, dx, dy, m);
+  else
+    clip_grad_by_norm_backward_cpu<T, false>(size, clip_norm_, dx, dy, m);
+}
+
+} // namespace nbla
