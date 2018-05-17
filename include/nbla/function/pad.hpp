344d3509eb30a41d66d39beca7ffa4e8d5cfc3b3
Poornima Biradar <poornima.biradar@sony.com>
2018-05-17 18:49:18 +0900

    Implement 3D padding for ND input.

--- include/nbla/function/pad.hpp
+++ include/nbla/function/pad.hpp
@@ -12,7 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-
 #ifndef NBLA_FUNCTION_PAD_HPP
 #define NBLA_FUNCTION_PAD_HPP
 
@@ -24,63 +23,87 @@ namespace nbla {
 
 NBLA_REGISTER_FUNCTION_HEADER(Pad, const vector<int> &, const string &, float);
 
-/** Pads given tensor with constant padding.
-len(pad_width)/2 represents the padding dimension.
+/** Pads given N-D array with specified sizes of dimensions.
 The dimensions that get padded begins with the last dimension and moves forward.
 
 Inputs:
 - x: N-D array.
-- pad_width: n-elem tuple, where n/2 ≤ input dimensions and n is even.
-- mode – ‘constant’, ‘reflect’ 'edge’. Default: ‘constant’
-- constant_value – fill value for ‘constant’ padding. Default: 0
+- pad_width: n-elem tuple, where n/2 <= input dimensions and n is even.
+  len(pad_width)/2 represents the padding dimension. (e.g. 1D, 2D, 3D etc.)
+  (Currently padding upto 3D is supported)
+- mode - Padding mode is one of the following. Default: constant.
+          1)constant : Elements in pad region are filled with constant_value.
+          2)replicate : Padded elements are filled with the values in nearest
+edges.
+          3)reflect : Padded with the reflection of the vector mirrored on the
+first and last values of the vector along each axis.
+          (Currently only constant mode is supported)
+- constant_value - Constant values filled in padded regions if mode is constant.
+Default: 0
 
 Outputs:
-- N-D array (B, C, H, W) where dimension depends on pad_width.
+- Padded N-D array (e.g. (B, C, H, W) shape) where dimension depends on
+pad_width.
+ndim() of output N-D array will be same as ndim() of input N-D array.
 for 1D padding:
-    3D input tensor with padding of the form (padLeft, padRight)
-    output tensor dimension (B, C, H, padLeft+W+padRight)
-for 2D:
-    4D input tensor with padding of the form (padTop, padBottom, padLeft, padRight).
-    output tensor dimension (B, C, padTop+H+padBottom, padLeft+W+padRight)
+    N-D input array with padding of the form (padLeft, padRight)
+    output N-D array dimension (B, C, H, padLeft + W + padRight)
+for 2D padding:
+    N-D input array with padding of the form (padTop, padBottom, padLeft,
+padRight).
+    output N-D array dimension (B, C, padTop + H + padBottom, padLeft + W +
+padRight)
+for 3D padding:
+    N-D input array with padding of the form (pasFront, padBack, padTop,
+padBottom, padLeft, padRight).
+    output N-D array dimension (B, padFront + C + padBack, padTop + H +
+padBottom, padLeft + W + padRight)
 
 @tparam T Data type for computation.
 \ingroup FunctionImplGrp
  */
 
-template <typename T> class Pad : public BaseFunction<const vector<int> &, const string &, float> {
+template <typename T>
+class Pad : public BaseFunction<const vector<int> &, const string &, float> {
 protected:
   const vector<int> pad_width_;
   const string mode_;
   float constant_value_;
+
 public:
-  Pad(const Context &ctx, const vector<int> & pad_width, const string & mode, float constant_value) : BaseFunction(ctx, pad_width, mode, constant_value)
-  , pad_width_(pad_width)
-  , mode_(mode)
-  , constant_value_(constant_value)
-    {}
+  Pad(const Context &ctx, const vector<int> &pad_width, const string &mode,
+      float constant_value)
+      : BaseFunction(ctx, pad_width, mode, constant_value),
+        pad_width_(pad_width), mode_(mode), constant_value_(constant_value) {
+    pad_mode_["constant"] = p_constant;
+    pad_mode_["replicate"] = p_replicate;
+    pad_mode_["reflect"] = p_reflect;
+  }
   virtual ~Pad() {}
   virtual shared_ptr<Function> copy() const {
     return create_Pad(ctx_, pad_width_, mode_, constant_value_);
   }
   virtual int min_inputs() { return 1; }
   virtual int min_outputs() { return 1; }
-  virtual vector<dtypes> in_types() {
-    return vector<dtypes>{get_dtype<T>()};
-  }
-  virtual vector<dtypes> out_types() {
-    return vector<dtypes>{get_dtype<T>()};
-  }
+  virtual vector<dtypes> in_types() { return vector<dtypes>{get_dtype<T>()}; }
+  virtual vector<dtypes> out_types() { return vector<dtypes>{get_dtype<T>()}; }
   virtual vector<string> allowed_array_classes() {
     return SingletonManager::get<Cpu>()->array_classes();
   }
   virtual string name() { return "Pad"; }
 
+  typedef enum pad_mode { p_constant, p_replicate, p_reflect } pad_mode;
+  std::map<std::string, pad_mode> pad_mode_;
+
 protected:
-  NBLA_API virtual void setup_impl(const Variables &inputs, const Variables &outputs);
-  NBLA_API virtual void forward_impl(const Variables &inputs, const Variables &outputs);
-  NBLA_API virtual void backward_impl(const Variables &inputs, const Variables &outputs,
-				      const vector<bool> &propagate_down,
-				      const vector<bool> &accum);
+  NBLA_API virtual void setup_impl(const Variables &inputs,
+                                   const Variables &outputs);
+  NBLA_API virtual void forward_impl(const Variables &inputs,
+                                     const Variables &outputs);
+  NBLA_API virtual void backward_impl(const Variables &inputs,
+                                      const Variables &outputs,
+                                      const vector<bool> &propagate_down,
+                                      const vector<bool> &accum);
 };
 }
 #endif
