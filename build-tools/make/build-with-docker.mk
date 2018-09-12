908f6cc7437e7d81ed5b9f07a98418026f31a450
Yukio Oobuchi <Yukio.Oobuchi@sony.com>
2018-09-12 13:43:53 +0900

    Build docker image

--- build-tools/make/build-with-docker.mk
+++ build-tools/make/build-with-docker.mk
@@ -125,11 +125,16 @@ bwd-nnabla-shell: docker_image_build
 ########################################################################################################################
 # Docker image with current nnabla
 .PHONY: docker_image_nnabla
-docker_image_nnabla: bwd-nnabla-cpplib bwd-nnabla-wheel
+docker_image_nnabla:
+	rm -rf $(NNABLA_DIRECTORY)/build_sdeepconsole
+	cp -rf output/build_sdeepconsole $(NNABLA_DIRECTORY)
 	docker pull ubuntu:16.04
 	cd $(NNABLA_DIRECTORY) \
-	&& cp docker/development/Dockerfile.build.py$(PYTHON_VERSION_MAJOR)$(PYTHON_VERSION_MINOR) Dockerfile \
+	&& cp docker/development/Dockerfile.build Dockerfile \
 	&& echo ADD $(shell echo build_wheel_py$(PYTHON_VERSION_MAJOR)$(PYTHON_VERSION_MINOR)/dist/*.whl) /tmp/ >>Dockerfile \
 	&& echo RUN pip install /tmp/$(shell basename build_wheel_py$(PYTHON_VERSION_MAJOR)$(PYTHON_VERSION_MINOR)/dist/*.whl) >>Dockerfile \
+	&& echo ADD build_sdeepconsole/settings /usr/local/bin/settings/ >>Dockerfile \
+	&& echo ADD build_sdeepconsole/sdeep_console_cli_util /usr/local/bin >>Dockerfile \
+	&& echo RUN chmod a+x /usr/local/bin/sdeep_console_cli_util >>Dockerfile \
 	&& docker build $(DOCKER_BUILD_ARGS) -t $(DOCKER_IMAGE_NNABLA) . \
 	&& rm -f Dockerfile
