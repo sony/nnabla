60334ff4476a5dc1eb93a717d34af0abb5992db8
Yukio Oobuchi <Yukio.Oobuchi@sony.com>
2018-09-20 13:15:08 +0900

    Revert parameter.py

--- python/src/nnabla/parameter.py
+++ python/src/nnabla/parameter.py
@@ -268,15 +268,13 @@ def set_parameter_from_proto(proto):
         var.need_grad = parameter.need_grad
 
 
-def load_parameters(path, proto=None, proto_only=False):
+def load_parameters(path):
     """Load parameters from a file with the specified format.
 
     Args:
       path : path or file object
     """
     _, ext = os.path.splitext(path)
-    if proto is None:
-        proto = nnabla_pb2.NNablaProtoBuf()
     if ext == '.h5':
         # TODO temporary work around to suppress FutureWarning message.
         import warnings
@@ -295,16 +293,11 @@ def load_parameters(path, proto=None, proto_only=False):
             hd.visit(_get_keys)
             for _, key in sorted(keys):
                 ds = hd[key]
-                if not proto_only:
-                    var = get_parameter_or_create(key, ds.shape,
-                                                  need_grad=ds.attrs['need_grad'])
-                    var.data.cast(ds.dtype)[...] = ds[...]
-                parameter = proto.parameter.add()
-                parameter.variable_name = key
-                parameter.shape.dim.extend(ds.shape)
-                parameter.data.extend(numpy.array(ds[...]).flatten().tolist())
-                parameter.need_grad = ds.attrs['need_grad'] is True
+                var = get_parameter_or_create(key, ds.shape,
+                                              need_grad=ds.attrs['need_grad'])
+                var.data.cast(ds.dtype)[...] = ds[...]
     elif ext == '.protobuf':
+        proto = nnabla_pb2.NNablaProtoBuf()
         with open(path, 'rb') as f:
             proto.MergeFromString(f.read())
             set_parameter_from_proto(proto)
@@ -322,12 +315,10 @@ def load_parameters(path, proto=None, proto_only=False):
                     nnp.extract(name, tmpdir)
                     _, ext = os.path.splitext(name)
                     if ext in ['.protobuf', '.h5']:
-                        proto = load_parameters(os.path.join(
-                            tmpdir, name), proto, proto_only)
+                        load_parameters(os.path.join(tmpdir, name))
         finally:
             shutil.rmtree(tmpdir)
     logger.info("Parameter load ({}): {}".format(format, path))
-    return proto
 
 
 def save_parameters(path):
