d74284b90ff059ff7eb79beb177fa8f4c5ae6461
Naoki Ide <Naoki.Ide@sony.com>
2018-10-31 11:05:18 +0900

    added examples/cpp/mnist_collection
    added examples/cpp/mnist_collection/CMakeLists.txt
    
    added examples/cpp/mnist_collection/GNUmakefile
    
    added examples/cpp/mnist_collection/mnist_data.hpp
    
    added examples/cpp/mnist_collection/train_lenet_classifier.cpp
    
    added examples/cpp/mnist_collection/lenet_training.hpp
    
    added examples/cpp/mnist_collection/train_resnet_classifier.cpp
    
    added examples/cpp/mnist_collection/resnet_training.hpp
    
    added examples/cpp/mnist_collection/train_vae.cpp
    
    added examples/cpp/mnist_collection/vae_training.hpp
    
    added examples/cpp/mnist_collection/train_dcgan.cpp
    
    added examples/cpp/mnist_collection/dcgan_training.hpp
    
    added examples/cpp/mnist_collection/train_siamese.cpp
    
    added examples/cpp/mnist_collection/siamese_training.hpp
    
    added examples/cpp/mnist_collection/train_vat.cpp
    
    added examples/cpp/mnist_collection/vat_training.hpp
    
    added examples/cpp/mnist_collection/README.md

---
+++ examples/cpp/mnist_collection/vae_training.hpp
@@ -0,0 +1,186 @@
+// Copyright (c) 2018 Sony Corporation. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// Include nnabla header files
+
+#include <fstream>
+#include <iostream>
+#include <random>
+#include <stdio.h>
+#include <string>
+#include <vector>
+using namespace std;
+
+#include <nbla/computation_graph/computation_graph.hpp>
+#include <nbla/solver/adam.hpp>
+using namespace nbla;
+using std::make_shared;
+
+#include <nbla/functions.hpp>
+#include <nbla/global_context.hpp>
+#include <nbla/parametric_functions.hpp>
+namespace f = nbla::functions;
+namespace pf = nbla::parametric_functions;
+
+#include "mnist_data.hpp"
+
+/******************************************/
+// Example of Lenet Model Design
+/******************************************/
+CgVariablePtr model(CgVariablePtr x, ParameterDirectory parameters) {
+
+  //############################################
+  // Encoder of 2 fully connected layers
+  //############################################
+
+  // Normalize input
+  auto xa = x;
+
+  // Fully connected layers, and Elu replaced from original Softplus.
+  auto h1 = f::elu(pf::affine(xa, 1, 500, parameters["fc1"]), 1.0);
+  auto h2 = f::elu(pf::affine(h1, 1, 500, parameters["fc2"]), 1.0);
+
+  // The outputs are the parameters of Gauss probability density.
+  auto mu = pf::affine(h2, 1, 50, parameters["fc_mu"]);
+  auto logvar = pf::affine(h2, 1, 50, parameters["fc_logvar"]);
+  auto sigma = f::exp(logvar * 0.5);
+
+  // The prior variable and the reparameterization trick
+  Shape_t shape_mu = mu->variable()->shape();
+  vector<int> shape_z(shape_mu.size());
+  for (int i = 0; i < shape_z.size(); i++)
+    shape_z[i] = shape_mu[i];
+  auto epsilon = f::randn(0.0, 1.0, shape_z, 706);
+  auto z = mu + sigma * epsilon;
+
+  //#############################################
+  //  Decoder of 2 fully connected layers       #
+  //#############################################
+
+  // Fully connected layers, and Elu replaced from original Softplus.
+  auto h3 = f::elu(pf::affine(z, 1, 500, parameters["fc3"]), 1.0);
+  auto h4 = f::elu(pf::affine(h3, 1, 500, parameters["fc4"]), 1.0);
+
+  // The outputs are the parameters of Bernoulli probabilities for each pixel.
+  Shape_t shape_xa = xa->variable()->shape();
+  int n_pb = 1;
+  vector<int> shape_pb(shape_xa.size());
+  for (int i = 0; i < shape_xa.size(); i++) {
+    if (0 < i)
+      n_pb *= shape_xa[i];
+    shape_pb[i] = shape_xa[i];
+  }
+  auto h5 = pf::affine(h4, 1, n_pb, parameters["fc5"]);
+  auto prob = f::reshape(h5, shape_pb, true);
+
+  //############################################
+  // Elbo components and loss objective        #
+  //############################################
+
+  // Binarized input
+  auto xb = f::greater_equal_scalar(xa, 0.5);
+
+  // E_q(z|x)[log(q(z|x))]
+  // without some constant terms that will canceled after summation of loss
+  auto logqz = 0.5 * f::sum(1.0 + logvar, {1}, false);
+
+  // E_q(z|x)[log(p(z))]
+  // without some constant terms that will canceled after summation of loss
+  auto logpz = 0.5 * f::sum(mu * mu + sigma * sigma, {1}, false);
+
+  // E_q(z|x)[log(p(x|z))]
+  auto logpx = f::sum(f::sigmoid_cross_entropy(prob, xb), {1, 2, 3}, false);
+
+  // Vae loss, the negative evidence lowerbound
+  auto loss = f::mean(logpx + logpz - logqz, {0}, false);
+
+  return loss;
+}
+
+/******************************************/
+// Example of Lenet Classifier Training
+/******************************************/
+bool vae_training(nbla::Context ctx) {
+
+  // Create a context for cpu
+  nbla::Context cpu_ctx{{"cpu:float"}, "CpuCachedArray", "0"};
+
+  // Create mnist data iterator
+  MnistDataIterator train_data_provider("train");
+  MnistDataIterator test_data_provider("test");
+
+  // Build network
+  SingletonManager::get<GlobalContext>()->set_current_context(ctx);
+  ParameterDirectory params;
+  int batch_size = 100;
+  auto x = make_shared<CgVariable>(Shape_t({batch_size, 1, 28, 28}), false);
+  auto t = make_shared<CgVariable>(Shape_t({batch_size, 1}), false);
+  auto loss = model(x, params);
+
+  // Setup solver and input learnable parameters
+  float learning_rate = 3.0e-4;
+  auto adam = create_AdamSolver(ctx, learning_rate, 0.9, 0.999, 1.0e-8);
+  adam->set_parameters(params.get_parameters());
+
+  // Execute training
+  FILE *fp;
+  fp = fopen("log.txt", "wt");
+  if (fp == NULL) {
+    fprintf(stderr, "Error in opening log file.");
+    return false;
+  }
+
+  int max_iter = 60000;
+  int n_val_interval = 10;
+  int n_val_iter = 10;
+  float mean_loss_t = 0.;
+  for (int iter = 0; iter < max_iter; iter++) {
+
+    // Get batch and copy to input variables
+    train_data_provider.provide_data(cpu_ctx, batch_size, x, t);
+
+    // Execute forward, backward and update
+    adam->zero_grad();
+    loss->forward(/*clear_buffer=*/false, /*clear_no_need_grad=*/true);
+    loss->variable()->grad()->fill(1.0);
+    loss->backward(/*NdArrayPtr grad =*/nullptr,
+                   /*bool clear_buffer = */ true);
+    adam->update();
+
+    // Get and print the average loss
+    float_t *loss_d =
+        loss->variable()->cast_data_and_get_pointer<float_t>(cpu_ctx, false);
+    mean_loss_t += loss_d[0];
+
+    if ((iter + 1) % n_val_interval == 0) {
+
+      float mean_loss_v = 0.0;
+      for (int v_iter = 0; v_iter < n_val_iter; v_iter++) {
+        test_data_provider.provide_data(cpu_ctx, batch_size, x, t);
+        loss->forward(/*clear_buffer=*/true, /*clear_no_need_grad=*/true);
+        float_t *loss_d = loss->variable()->cast_data_and_get_pointer<float_t>(
+            cpu_ctx, false);
+        mean_loss_v += loss_d[0];
+      }
+      mean_loss_t /= n_val_interval;
+      mean_loss_v /= n_val_iter;
+
+      fprintf(fp, "iter: %d, tloss: %f, vloss: %f\n", iter + 0, mean_loss_t,
+              mean_loss_v);
+      fprintf(stdout, "iter: %d, tloss: %f, vloss: %f\n", iter + 0, mean_loss_t,
+              mean_loss_v);
+      mean_loss_t = 0;
+    }
+  }
+  fclose(fp);
+  return true;
+}
---
+++ examples/cpp/mnist_collection/vat_training.hpp
@@ -0,0 +1,236 @@
+// Copyright (c) 2019 Sony Corporation. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// Include nnabla header files
+
+#include <stdio.h>
+#include <string>
+#include <vector>
+using namespace std;
+
+#include <nbla/computation_graph/computation_graph.hpp>
+#include <nbla/solver/adam.hpp>
+using namespace nbla;
+using std::make_shared;
+#include <nbla/functions.hpp>
+#include <nbla/global_context.hpp>
+#include <nbla/parametric_functions.hpp>
+namespace f = nbla::functions;
+namespace pf = nbla::parametric_functions;
+
+#include "mnist_data.hpp"
+std::random_device seed_gen;
+std::default_random_engine engine(seed_gen());
+std::uniform_real_distribution<> uniform(0.0, 1.0);
+std::normal_distribution<> normal(0.0, 1.0);
+
+/******************************************/
+// Example of VAT Model
+/******************************************/
+CgVariablePtr mlp_net(CgVariablePtr x, int n_h, int n_y,
+                      ParameterDirectory params, bool test) {
+  // from python examples
+  //"""
+  // Function for building multi-layer-perceptron with batch_normalization
+  //
+  // Args:
+  //    x(`~nnabla.Variable`): N-D array
+  //    n_h(int): number of units in an intermediate layer
+  //    n_y(int): number of classes
+  //    test: operation type train=True, test=False
+  //
+  // Returns:
+  //    ~nnabla.Variable: h
+  //"""
+  auto h1 = pf::affine(x, 1, n_h, params["fc1"]);
+  auto r1 = f::relu(pf::batch_normalization(h1, !test, params["fc1"]), false);
+  auto h2 = pf::affine(r1, 1, n_h, params["fc2"]);
+  auto r2 = f::relu(pf::batch_normalization(h2, !test, params["fc3"]), false);
+  auto h3 = pf::affine(r2, 1, n_y, params["fc3"]);
+  return h3;
+}
+
+CgVariablePtr distance(CgVariablePtr y0, CgVariablePtr y1) {
+  // from python examples
+  // def distance(y0, y1):
+  //"""
+  // Distance function is Kullback-Leibler Divergence for categorical
+  // distribution
+  //"""
+  return f::kl_multinomial(f::softmax(y0, 1), f::softmax(y1, 1), 1);
+}
+
+/******************************************/
+// Example of Virtual Adversarial Training
+/******************************************/
+bool vat_training(nbla::Context ctx) {
+  // from python examples
+  //"""
+  // Main script.
+  //
+  // Steps:
+  //  * Get and set context.
+  //  * Load Dataset
+  //  * Initialize DataIterator.
+  //  * Create Networks
+  //  *   Network for training with labeled data
+  //  *   Network for training with unlabeled data
+  //  *   Network for evaluation with validation data
+  //  * Create Solver.
+  //  * Training Loop.
+  //  *   Training
+  //  *     by Labeled Data
+  //  *       Calculate loss with labeled data
+  //  *     by Unlabeled Data
+  //  *       Calculate virtual adversarial loss
+  //  *       Calculate loss with virtual adversarial noise
+  //  """
+
+  // Create a context for cpu
+  nbla::Context cpu_ctx{{"cpu:float"}, "CpuCachedArray", "0"};
+
+  //  # Create networks
+  //  # Network
+  SingletonManager::get<GlobalContext>()->set_current_context(ctx);
+  ParameterDirectory params;
+  int n_h = 1200;
+  int n_y = 10;
+
+  //  # Network for training with labeled data
+  int batch_size_l = 100;
+  auto xl = make_shared<CgVariable>(Shape_t({batch_size_l, 1, 28, 28}), false);
+  auto tl = make_shared<CgVariable>(Shape_t({batch_size_l, 1}), false);
+  auto yl = mlp_net(xl, n_h, n_y, params, false);
+  auto loss_l = f::mean(f::softmax_cross_entropy(yl, tl, 1), {0, 1}, false);
+
+  //  # Network for training with unlabeled data
+  int batch_size_u = 250;
+  float xi = 10.0;
+  float eps = 1.5;
+  auto xu = make_shared<CgVariable>(Shape_t({batch_size_u, 1, 28, 28}), false);
+  auto tu = make_shared<CgVariable>(Shape_t({batch_size_u, 1}), false);
+  auto yu = mlp_net(xu, n_h, n_y, params, false);
+  auto y1 = make_shared<CgVariable>(yu->variable(), true);
+  y1->set_need_grad(false);
+
+  auto noise =
+      make_shared<CgVariable>(Shape_t({batch_size_u, 1, 28, 28}), true);
+  auto r = noise / f::pow_scalar(
+                       (f::sum(f::pow_scalar(noise, 2), {1, 2, 3}, true)), 0.5);
+  r->set_persistent(true);
+  auto y2 = mlp_net(xu + xi * r, n_h, n_y, params, false);
+  auto y3 = mlp_net(xu + eps * r, n_h, n_y, params, false);
+  auto loss_k = f::mean(distance(y1, y2), {0}, false);
+  auto loss_u = f::mean(distance(y1, y3), {0}, false);
+
+  //  # Network for evaluation with validation data
+  int batch_size_v = 10000;
+  auto xv = make_shared<CgVariable>(Shape_t({batch_size_v, 1, 28, 28}), false);
+  auto hv = mlp_net(xv, n_h, n_y, params, true);
+  auto tv = make_shared<CgVariable>(Shape_t({batch_size_v, 1}), false);
+  auto err = f::mean(f::top_n_error(hv, tv, 1, 1), {0, 1}, false);
+
+  //  # Solver.
+  float learning_rate = 2.0e-3;
+  auto solver = create_AdamSolver(ctx, learning_rate, 0.9, 0.999, 1.0e-8);
+  solver->set_parameters(params.get_parameters());
+
+  //  # Training loop.
+  MnistDataIterator train_data_iterator("train");
+  MnistDataIterator valid_data_iterator("valid");
+  auto labeled_set = train_data_iterator.get_batch(100);
+  int max_iter = 24000;
+  float learning_rate_decay = 0.9;
+  int learning_rate_decay_interval = 240;
+  float weight_decay = 0.0e-4;
+  int max_iter_power_method = 1;
+  int n_val_interval = 240;
+  float mean_tloss = 0.;
+  FILE *fp;
+  fp = fopen("log.txt", "wt");
+  if (fp == NULL) {
+    fprintf(stderr, "Error in opening log file.");
+    return false;
+  }
+  for (int iter = 0; iter < max_iter; iter++) {
+    // Training with labeled data
+    set_data(cpu_ctx, labeled_set, xl, tl);
+    solver->zero_grad();
+    loss_l->forward(/*clear_buffer=*/false, /*clear_no_need_grad=*/true);
+    loss_l->variable()->grad()->fill(1.0);
+    loss_l->backward(/*NdArrayPtr grad=*/nullptr, /*clear_buffer=*/true);
+    solver->weight_decay(weight_decay);
+    solver->update();
+
+    // Training with unlabeled data
+    train_data_iterator.provide_data(cpu_ctx, batch_size_u, xu, tu);
+    yu->forward(/*clear_buffer=*/false, /*clear_no_need_grad=*/false);
+
+    // Calculating virtual adversarial noise
+    float_t *n_d =
+        noise->variable()->cast_data_and_get_pointer<float_t>(cpu_ctx, true);
+    for (int i = 0; i < noise->variable()->size(); i++, n_d++)
+      *n_d = normal(engine);
+
+    for (int k = 0; k < max_iter_power_method; k++) {
+      r->variable()->grad()->fill(0.0);
+      loss_k->forward(/*clear_buffer=*/false, /*clear_no_need_grad=*/true);
+      loss_k->variable()->grad()->fill(1.0);
+      loss_k->backward(/*NdArrayPtr grad=*/nullptr, /*clear_buffer=*/true);
+
+      n_d =
+          noise->variable()->cast_data_and_get_pointer<float_t>(cpu_ctx, true);
+      float_t *r_g =
+          r->variable()->cast_grad_and_get_pointer<float_t>(cpu_ctx, false);
+      for (int i = 0; i < noise->variable()->size(); i++, n_d++, r_g++)
+        *n_d = *r_g;
+    }
+
+    // Updating with virtual adversarial noise
+    solver->zero_grad();
+    loss_u->forward(/*clear_buffer=*/false, /*clear_no_need_grad=*/true);
+    loss_u->variable()->grad()->fill(1.0);
+    loss_u->backward(/*NdArrayPtr grad=*/nullptr, /*clear_buffer=*/true);
+    solver->weight_decay(weight_decay);
+    solver->update();
+
+    //      ##### Learning rate update #####
+    if (iter % learning_rate_decay_interval == 0) {
+      solver->set_learning_rate(solver->learning_rate() * learning_rate_decay);
+    }
+
+    // Monitor loss and error rate
+    float_t *loss_l_d =
+        loss_l->variable()->cast_data_and_get_pointer<float_t>(cpu_ctx, false);
+    float_t *loss_u_d =
+        loss_u->variable()->cast_data_and_get_pointer<float_t>(cpu_ctx, false);
+    mean_tloss += *loss_l_d + *loss_u_d;
+
+    if ((iter + 1) % n_val_interval == 0) {
+
+      mean_tloss /= n_val_interval;
+
+      valid_data_iterator.provide_data(cpu_ctx, batch_size_v, xv, tv);
+      err->forward(/*clear_buffer=*/false, /*clear_no_need_grad=*/true);
+      float_t *err_d =
+          err->variable()->cast_data_and_get_pointer<float_t>(cpu_ctx, false);
+
+      fprintf(fp, "iter: %d, tloss: %f, verr: %f\n", iter + 1, mean_tloss,
+              *err_d);
+      fprintf(stdout, "iter: %d, tloss: %f, verr: %f\n", iter + 1, mean_tloss,
+              *err_d);
+      mean_tloss = 0;
+    }
+  }
+  fclose(fp);
+  return true;
+}
