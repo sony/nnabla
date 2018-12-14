09cbcdc908ecb3b03f29451ff395d599b5def80f
Takuya.Yashima <Takuya.Yashima@sony.com>
2018-12-14 16:12:09 +0900

    modified clip_by_norm

--- python/test/function/test_clip_by_norm.py
+++ python/test/function/test_clip_by_norm.py
@@ -21,21 +21,54 @@ from nbla_test_utils import list_context
 
 def ref_clip_by_norm(x, clip_norm, axis):
     x_norm = np.sqrt(np.sum(x ** 2.0, axis=axis, keepdims=True))
-    x = clip_norm * x / x_norm
+    x = clip_norm * x / np.maximum(x_norm, clip_norm)
     return x
 
 
 @pytest.mark.parametrize("seed", [313])
-@pytest.mark.parametrize("clip_norm", [0.5, ])
+@pytest.mark.parametrize("clip_norm_type", ["float", "int", "Variable"])
+@pytest.mark.parametrize("x_value", ["non_zero", "all_zero"])
+@pytest.mark.parametrize("clip_norm_value", [1.5, 0.5, 1e+5, -3.0, 0.0])
 @pytest.mark.parametrize("shape, axis", [((2, 8, 8, 8), (2, 3)),
                                          ((2, 8, 8, 8), (1, )),
                                          ((2, 4), (1, )),
                                          ((2, 4, 4), None)])
-def test_clip_by_norm_forward(seed, shape, clip_norm, axis):
+def test_clip_by_norm_forward(seed, shape, clip_norm_type, clip_norm_value, x_value, axis):
+
+    def execute_clip_by_norm(x, x_data, clip_norm, clip_norm_value, axis):
+        if isinstance(clip_norm, (nn.Variable, nn.NdArray)):
+            if clip_norm_value <= 0:
+                pytest.skip()
+            else:
+                with nn.auto_forward(True):
+                    y = F.clip_by_norm(x, clip_norm, axis)
+                y_ref = ref_clip_by_norm(x_data, clip_norm_value, axis=axis)
+                assert np.allclose(y.d, y_ref)
+        else:
+            if clip_norm_value > 0:
+                with nn.auto_forward(True):
+                    y = F.clip_by_norm(x, clip_norm, axis)
+                y_ref = ref_clip_by_norm(x_data, clip_norm_value, axis=axis)
+                assert np.allclose(y.d, y_ref)
+            else:
+                with pytest.raises(ValueError):
+                    y = F.clip_by_norm(x, clip_norm, axis)
+
     rng = np.random.RandomState(seed)
     x_data = rng.randn(*shape)
     x = nn.Variable.from_numpy_array(x_data)
-    with nn.auto_forward(True):
-        y = F.clip_by_norm(x, clip_norm, axis)
-    y_ref = ref_clip_by_norm(x_data, clip_norm, axis=axis)
-    assert np.allclose(y.d, y_ref)
+
+    if clip_norm_type == "float":
+        clip_norm = clip_norm_value
+    elif clip_norm_type == "int":
+        clip_norm_value = int(clip_norm_value)
+        clip_norm = clip_norm_value
+    else:
+        clip_norm = nn.Variable(x.ndim*(1,))
+        clip_norm.d = clip_norm_value
+
+    if x_value == "all_zero":
+        x.d = 0
+        x_data = np.zeros(x.shape)
+
+    execute_clip_by_norm(x, x_data, clip_norm, clip_norm_value, axis)
