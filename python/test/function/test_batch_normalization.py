c169843030f2a2cc4f26392e039bacdd78d60677
TE-BasavarajMurali <Basavaraj.Murali@sony.com>
2019-03-19 15:52:05 +0900

    Support BatchNormalization for batch_stat=False

--- python/test/function/test_batch_normalization.py
+++ python/test/function/test_batch_normalization.py
@@ -51,8 +51,8 @@ def create_inputs(rng, axis):
     shape_stat[axis] = x.shape[axis]
     beta = rng.randn(*shape_stat).astype(np.float32)
     gamma = rng.randn(*shape_stat).astype(np.float32)
-    rmean = np.zeros(shape_stat, dtype=np.float32)
-    rvar = np.zeros(shape_stat, dtype=np.float32)
+    rmean = rng.randn(*shape_stat).astype(np.float32)
+    rvar = 1 + rng.rand(*shape_stat).astype(np.float32)
     return x, beta, gamma, rmean, rvar
 
 
@@ -60,20 +60,24 @@ def create_inputs(rng, axis):
 @pytest.mark.parametrize("axis", [0, 1, 2])
 @pytest.mark.parametrize("decay_rate", [0.9])
 @pytest.mark.parametrize("eps", [1e-5])
-@pytest.mark.parametrize("output_stat", [True, False])
+@pytest.mark.parametrize("output_stat, batch_stat", [[False, False], [False, True], [True, True]])
 @pytest.mark.parametrize("ctx, func_name", ctxs)
 def test_batch_normalization_forward_backward(seed, axis, decay_rate, eps,
-                                              output_stat, ctx, func_name):
+                                              output_stat, batch_stat, ctx, func_name):
     from nbla_test_utils import function_tester
     rng = np.random.RandomState(seed)
     inputs = list(create_inputs(rng, axis))
     axes = [axis]
-    batch_stat = True
-    function_tester(rng, F.batch_normalization, ref_batch_normalization,
-                    inputs,
-                    func_args=[axes, decay_rate, eps, batch_stat, output_stat],
-                    backward=[True, True, True, False, False],
-                    ctx=ctx, func_name=func_name, dstep=1e-2, atol_b=1e-2)
+    if ctx.backend[0].split(':')[0] != 'cpu' and batch_stat == False:
+        pytest.skip(
+            "cuda and cudnn implementation for batch_stat==False is not implemented yet")
+    else:
+        function_tester(rng, F.batch_normalization, ref_batch_normalization,
+                        inputs,
+                        func_args=[axes, decay_rate, eps,
+                                   batch_stat, output_stat],
+                        backward=[True, True, True, False, False],
+                        ctx=ctx, func_name=func_name, dstep=1e-2, atol_b=1e-2)
 
     # Check if running mean and var works.
     vinputs = []
@@ -88,16 +92,5 @@ def test_batch_normalization_forward_backward(seed, axis, decay_rate, eps,
         with nn.context_scope(ctx), nn.auto_forward():
             y = F.batch_normalization(
                 *(vinputs + [axes, decay_rate, eps, batch_stat, output_stat]))
-        assert np.allclose(vinputs[3].d, inputs[3])
-        assert np.allclose(vinputs[4].d, inputs[4], atol=1e-3)
-
-    # Check if global stat mode works
-    batch_stat = False
-    if output_stat:
-        return
-    ref_y = ref_batch_normalization(
-        *(inputs + [axes, decay_rate, eps, batch_stat, output_stat]))
-    with nn.context_scope(ctx), nn.auto_forward():
-        y = F.batch_normalization(
-            *(vinputs + [axes, decay_rate, eps, batch_stat, output_stat]))
-    assert np.allclose(ref_y, y.d, atol=1e-6)
+        assert np.allclose(vinputs[3].d, inputs[3], atol=1e-7)
+        assert np.allclose(vinputs[4].d, inputs[4])
