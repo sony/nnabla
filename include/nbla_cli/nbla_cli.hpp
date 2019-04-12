c2a33b223e48d27f4519aca80c41aaa3f1c9ef5c
Woody.Li <Woody.Li@sony.com>
2019-04-12 10:40:39 +0800

    separate implementation from its wraper for sharing

---
+++ include/nbla_cli/nbla_cli.hpp
@@ -0,0 +1,22 @@
+// Copyright (c) 2019 Sony Corporation. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef NBLA_CLI_HPP_
+#define NBLA_CLI_HPP_
+
+#include <nbla_cli/nbla_dump.h>
+#include <nbla_cli/nbla_infer.h>
+#include <nbla_cli/nbla_train.h>
+
+#endif // NBLA_CLI_HPP_
