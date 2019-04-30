719d2e9434c07bf0540318e8fcf0c8c54d630334
woody.li <woody.li@sony.com>
2019-04-30 11:20:32 +0800

    add dicom support

--- python/src/nnabla/utils/image_utils/dicom_utils.py
+++ python/src/nnabla/utils/image_utils/dicom_utils.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2017 Sony Corporation. All Rights Reserved.
+# Copyright (c) 2019 Sony Corporation. All Rights Reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -13,32 +13,25 @@
 # limitations under the License.
 
 from __future__ import absolute_import, division
-
-import cv2
+import pydicom
+from pydicom.dataset import Dataset, FileDataset
 import numpy as np
+import datetime
+import random
+import cv2
 
 from nnabla.logger import logger
 from .common import upscale_pixel_intensity, check_type_and_cast_if_necessary,\
-    _imread_before, _imread_after, _imsave_before, _imresize_before, _imresize_after
-
-interpolations_map = {
-    "nearest": cv2.INTER_NEAREST,
-    "bilinear": cv2.INTER_LINEAR,
-    "bicubic": cv2.INTER_CUBIC,
-    "lanczos": cv2.INTER_LANCZOS4,
-}
+    _imread_before, _imread_after, _imsave_before
 
 
-def _imread_helper(path, r_mode):
-    if hasattr(path, "read"):
-        img = cv2.imdecode(np.asarray(bytearray(path.read())), r_mode)
-    else:
-        img = cv2.imread(path, r_mode)
+DEFAULT_LITTLE_ENDIAN = True
 
-    return img
+# Using cv2.resize, since only cv2.resize support int16
+from .cv2_utils import imresize
 
 
-def convert_channel_from_gray(img, num_channels):
+def _convert_channel_from_gray(img, num_channels):
     if num_channels in [-1, 0]:
         return img
 
@@ -54,36 +47,36 @@ def convert_channel_from_gray(img, num_channels):
     raise ValueError("num_channels must be [-1, 0, 1, 3, 4]")
 
 
-def convert_channel_from_bgr(img, num_channels):
+def _convert_channel_from_rgb(img, num_channels):
     if num_channels in [0, 1]:
-        img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        img = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
         if num_channels == 1:
             img = img[..., np.newaxis]
 
         return img
 
-    elif num_channels in [-1, 3]:  # BGR => RGB
-        return cv2.cvtColor(img, cv2.COLOR_BGR2RGB, dst=img)
+    elif num_channels in [-1, 3]:  # RGB => RGB
+        return img
 
-    elif num_channels == 4:  # BGR => RGBA
-        return cv2.cvtColor(img, cv2.COLOR_BGR2RGBA)
+    elif num_channels == 4:  # RGB => RGBA
+        return cv2.cvtColor(img, cv2.COLOR_RGB2RGBA)
 
     raise ValueError("num_channels must be [-1, 0, 1, 3, 4]")
 
 
-def convert_channel_from_bgra(img, num_channels):
+def _convert_channel_from_rgba(img, num_channels):
     if num_channels in [0, 1]:
-        img = cv2.cvtColor(img, cv2.COLOR_BGRA2GRAY)
+        img = cv2.cvtColor(img, cv2.COLOR_RGBA2GRAY)
         if num_channels == 1:
             img = img[..., np.newaxis]
 
         return img
 
-    elif num_channels == 3:  # BGRA => RGB
-        return cv2.cvtColor(img, cv2.COLOR_BGRA2RGB)
+    elif num_channels == 3:
+        return cv2.cvtColor(img, cv2.COLOR_RGBA2RGB)
 
-    elif num_channels in [-1, 4]:  # BGRA => RGBA
-        return cv2.cvtColor(img, cv2.COLOR_BGRA2RGBA, dst=img)
+    elif num_channels in [-1, 4]:  # RGBA => RGBA
+        return img
 
     raise ValueError("num_channels must be [-1, 0, 1, 3, 4]")
 
@@ -91,13 +84,13 @@ def convert_channel_from_bgra(img, num_channels):
 def _cvtColor_helper(img, num_channels):
     # convert num_channels which means (h, w, c) -> (h, w, num_channels)
     if len(img.shape) == 2:
-        return convert_channel_from_gray(img, num_channels)
+        return _convert_channel_from_gray(img, num_channels)
 
     elif img.shape[-1] == 3:
-        return convert_channel_from_bgr(img, num_channels)
+        return _convert_channel_from_rgb(img, num_channels)
 
     elif img.shape[-1] == 4:
-        return convert_channel_from_bgra(img, num_channels)
+        return _convert_channel_from_rgba(img, num_channels)
 
     raise ValueError("Bad image shape. ({})".format(img.shape))
 
@@ -105,8 +98,9 @@ def _cvtColor_helper(img, num_channels):
 def imread(path, grayscale=False, size=None, interpolate="bilinear",
            channel_first=False, as_uint16=False, num_channels=-1):
     """
-    Read image by cv2 module.
+    Read image by pydicom module.
 
+    Before using pydicom module, opencv needs to be installed.
     Args:
         path (str or 'file object'): File path or object to read.
         grayscale (bool):
@@ -119,7 +113,7 @@ def imread(path, grayscale=False, size=None, interpolate="bilinear",
         interpolate (str):
             must be one of ["nearest", "bilinear", "bicubic", "lanczos"].
         as_uint16 (bool):
-            If True, this function reads image as uint16.
+            If you specify this argument, you can use only False for pil backend.
         num_channels (int):
             channel size of output array.
             Default is -1 which preserves raw image shape.
@@ -127,12 +121,11 @@ def imread(path, grayscale=False, size=None, interpolate="bilinear",
     Returns:
          numpy.ndarray
     """
-
     _imread_before(grayscale, num_channels)
 
-    r_mode = cv2.IMREAD_GRAYSCALE if grayscale else cv2.IMREAD_UNCHANGED
-    img = _imread_helper(path, r_mode)
+    dicom_dataset = pydicom.dcmread(path)
 
+    img = dicom_dataset.pixel_array
     if as_uint16 and img.dtype != np.uint16:
         if img.dtype == np.uint8:
             logger.warning("You want to read image as uint16, but the original bit-depth is 8 bit."
@@ -142,79 +135,62 @@ def imread(path, grayscale=False, size=None, interpolate="bilinear",
             raise ValueError(
                 "casting {} to uint16 is not safe.".format(img.dtype))
 
+    if num_channels == -1 and grayscale:
+        num_channels = 0
     img = _cvtColor_helper(img, num_channels)
 
-    img = _imread_after(img, size, interpolate, channel_first, imresize)
-
-    return img
+    return _imread_after(img, size, interpolate, channel_first, imresize)
 
 
 def imsave(path, img, channel_first=False, as_uint16=False, auto_scale=True):
     """
-    Save image by cv2 module.
+    Save image by dicom module.
+
     Args:
         path (str): output filename
         img (numpy.ndarray): Image array to save. Image shape is considered as (height, width, channel) by default.
-        channel_first:
+        channel_first (bool):
             This argument specifies the shape of img is whether (height, width, channel) or (channel, height, width).
-            Default value is False, which means the img shape is (height, width, channel)
+            Default value is False, which means the img shape is considered as (height, width, channel)
         as_uint16 (bool):
-            If True, save image as uint16.
-         auto_scale (bool) :
+            In this backend, this argument is always False because pillow dose not support uint16.
+            If True, exception will be raised.
+        auto_scale (bool) :
             Whether upscale pixel values or not.
             If you want to save float image, this argument must be True.
-            In cv2 backend, all below are supported.
-                - float ([0, 1]) to uint8 ([0, 255])  (if img.dtype==float and auto_scale==True and as_uint16==False)
-                - float to uint16 ([0, 65535]) (if img.dtype==float and auto_scale==True and as_uint16==True)
-                - uint8 to uint16 are supported (if img.dtype==np.uint8 and auto_scale==True and as_uint16==True)
     """
-
     img = _imsave_before(img, channel_first, auto_scale)
-
     if auto_scale:
         img = upscale_pixel_intensity(img, as_uint16)
-
     img = check_type_and_cast_if_necessary(img, as_uint16)
 
-    # revert channel order to opencv`s one.
-    if len(img.shape) == 3:
-        if img.shape[-1] == 3:
-            img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
-        elif img.shape[-1] == 4:
-            img = cv2.cvtColor(img, cv2.COLOR_RGBA2BGRA)
+    file_meta = Dataset()
+    file_meta.MediaStorageSOPClassUID = '1.2.840.10008.5.1.4.1.1.2'
+    file_meta.MediaStorageSOPInstanceUID = "1.2.3"
+    file_meta.ImplementationClassUID = "1.2.3.4"
+    file_meta.TransferSyntaxUID = pydicom.uid.ImplicitVRLittleEndian
 
-    cv2.imwrite(path, img)
+    ds = FileDataset(path, {},
+                     file_meta=file_meta, preamble=b"\0" * 128)
 
+    ds.PatientName = "NNablaImageUtils^Autogen"
+    ds.PatientID = ''.join(random.choice('01234567890') for i in range(10))
 
-def imresize(img, size, interpolate="bilinear", channel_first=False):
-    """
-    Resize image by cv2 module.
-    Args:
-        img (numpy.ndarray): Image array to save.
-            Image shape is considered as (height, width, channel) for RGB or (height, width) for gray-scale by default.
-        size (tupple of int): (width, height).
-        channel_first: bool
-            This argument specifies the shape of img is whether (height, width, channel) or (channel, height, width).
-            Default value is False, which means the img shape is (height, width, channel)
-        interpolate (str):
-            must be one of ["nearest", "bilinear", "bicubic", "lanczos"]
-    Returns:
-         numpy.ndarray whose shape is (size[1], size[0], channel) or (size[1], size[0])
-    """
+    ds.is_little_endian = True
+    ds.is_implicit_VR = True
 
-    img = _imresize_before(img, size, channel_first,
-                           interpolate, list(interpolations_map.keys()))
+    dt = datetime.datetime.now()
+    ds.ContentDate = dt.strftime('%Y%m%d')
+    timeStr = dt.strftime('%H%M%S.%f')  # long format with micro seconds
+    ds.ContentTime = timeStr
+    if len(img.shape) == 2:
+        ds.Rows, ds.Columns = img.shape
+        ds.SamplesPerPixel = 1
+    else:
+        ds.Rows, ds.Columns, ds.SamplesPerPixel = img.shape
+    ds.PixelData = img.tobytes()
+    ds.BitsAllocated = 16 if as_uint16 else 8  # 8 or 16 bits
+    ds.PixelRepresentation = 0  # unsigned
+    ds.PlanarConfiguration = 0   # W, H, C style
 
-    dtype = img.dtype
-
-    expand_flag = len(img.shape) == 3 and img.shape[-1] == 1
-    # if img shape is (h, w, 1),
-    # cv2.reshape automatically shrink the last channel and the shape of returned img is (h, w)
-
-    resized = cv2.resize(
-        img, size, interpolation=interpolations_map[interpolate])
-
-    if expand_flag:
-        resized = np.expand_dims(resized, axis=-1)
-
-    return _imresize_after(resized, channel_first).astype(dtype)
+    ds.save_as(path)
