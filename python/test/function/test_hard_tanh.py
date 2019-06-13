[Fix] Derivatives for Activation Layers
fix more functions

temp

fix dy

fixed tests

 format

python/test/function/test_hard_tanh.py
@@ -25,8 +25,8 @@ def ref_hard_tanh(x):
     return np.maximum(-1, np.minimum(1, x))
 
 
-def ref_hard_tanh_backward(x, dx):
-    return np.array([1 if i <= 1 and i >= -1 else 0 for i in np.nditer(x)])
+def ref_hard_tanh_backward(x, dy):
+    return np.array([dy if i <= 1 and i >= -1 else 0 for i in np.nditer(x)])
 
 
 @pytest.mark.parametrize("ctx, func_name", ctxs)
@@ -34,6 +34,7 @@ def ref_hard_tanh_backward(x, dx):
 def test_hard_tanh_forward_backward(seed, ctx, func_name):
     from nbla_test_utils import cap_ignore_region, function_tester
     rng = np.random.RandomState(seed)
-    inputs = [rng.randn(2, 3, 4).astype(np.float32)]
+    inputs = [
+        np.clip(np.abs(rng.randn(2, 3, 4).astype(np.float32)) * 1e4, 1e-2, 1e4)]
     function_tester(rng, F.hard_tanh, ref_hard_tanh, inputs,
                     ctx=ctx, func_name=func_name, ref_grad=ref_hard_tanh_backward)

python/test/function/test_sinc.py
@@ -28,13 +28,6 @@ def ref_sinc(x):
         return np.sin(x)/x
 
 
-def ref_sinc_backward(x, dx):
-    if np.allclose(x, 0):
-        return 0
-    else:
-        return (np.cos(x)/x - np.sin(x)/(x*x)).flat
-
-
 @pytest.mark.parametrize("ctx, func_name", ctxs)
 @pytest.mark.parametrize("seed", [313])
 def test_sinc_forward_backward(seed, ctx, func_name):
@@ -42,4 +35,4 @@ def test_sinc_forward_backward(seed, ctx, func_name):
     rng = np.random.RandomState(seed)
     inputs = [rng.randn(2, 3, 4).astype(np.float32) * 1]
     function_tester(rng, F.sinc, ref_sinc, inputs,
-                    ctx=ctx, func_name=func_name, ref_grad=ref_sinc_backward)
+                    ctx=ctx, func_name=func_name)

python/test/function/test_tanh_shrink.py
@@ -25,10 +25,6 @@ def ref_tanh_shrink(x):
     return x-np.tanh(x)
 
 
-def ref_tanh_shrink_backward(x, dx):
-    return np.power(np.tanh(x), 2).flat
-
-
 @pytest.mark.parametrize("ctx, func_name", ctxs)
 @pytest.mark.parametrize("seed", [313])
 def test_tanh_shrink_forward_backward(seed, ctx, func_name):
@@ -36,4 +32,4 @@ def test_tanh_shrink_forward_backward(seed, ctx, func_name):
     rng = np.random.RandomState(seed)
     inputs = [rng.randn(2, 3, 4).astype(np.float32) * 1]
     function_tester(rng, F.tanh_shrink, ref_tanh_shrink, inputs,
-                    ctx=ctx, func_name=func_name, ref_grad=ref_tanh_shrink_backward)
+                    ctx=ctx, func_name=func_name)