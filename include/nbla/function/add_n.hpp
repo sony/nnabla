8c1ba3e395e81056b56abbfafd2c0f56680ed1d8
TE-BasavarajMurali <Basavaraj.Murali@sony.com>
2019-12-25 23:28:16 +0530

    CPU implementation of Add_n function

---
+++ include/nbla/function/add_n.hpp
@@ -0,0 +1,68 @@
+// Copyright (c) 2017 Sony Corporation. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef NBLA_FUNCTION_ADD_N_HPP
+#define NBLA_FUNCTION_ADD_N_HPP
+
+#include <nbla/cpu.hpp>
+#include <nbla/function.hpp>
+#include <nbla/function_registry.hpp>
+
+namespace nbla {
+
+NBLA_REGISTER_FUNCTION_HEADER(AddN);
+
+/** Elementwise add
+The function is defined as
+@f[
+y_i = x^{(0)}_i + . . . + x^{(n-1)}_i.
+@f]
+
+Inputs:
+- N-D arrays.
+
+Outputs:
+- N-D array.
+
+@tparam T Data type for computation.
+\ingroup FunctionImplGrp
+ */
+
+template <typename T> class AddN : public BaseFunction<> {
+protected:
+public:
+  AddN(const Context &ctx) : BaseFunction(ctx) {}
+  virtual ~AddN() {}
+  virtual shared_ptr<Function> copy() const { return create_AddN(ctx_); }
+  virtual int min_inputs() { return 1; }
+  virtual int min_outputs() { return 1; }
+  virtual vector<dtypes> in_types() { return vector<dtypes>{get_dtype<T>()}; }
+  virtual vector<dtypes> out_types() { return vector<dtypes>{get_dtype<T>()}; }
+  virtual vector<string> allowed_array_classes() {
+    return SingletonManager::get<Cpu>()->array_classes();
+  }
+  virtual string name() { return "AddN"; }
+
+protected:
+  NBLA_API virtual void setup_impl(const Variables &inputs,
+                                   const Variables &outputs);
+  NBLA_API virtual void forward_impl(const Variables &inputs,
+                                     const Variables &outputs);
+  NBLA_API virtual void backward_impl(const Variables &inputs,
+                                      const Variables &outputs,
+                                      const vector<bool> &propagate_down,
+                                      const vector<bool> &accum);
+};
+}
+#endif
