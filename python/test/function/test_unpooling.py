582c63f97fa015d42fc6a46ef55525ac8efc2e9d
TE-Kazuki.Yoshiyama <Kazuki.Yoshiyama@sony.com>
2020-04-07 14:59:32 +0200

    Unpooling channel last.
    Unpooling channel last.
    
    Channel-included test cases.
    
    Format.

--- python/test/function/test_unpooling.py
+++ python/test/function/test_unpooling.py
@@ -15,15 +15,22 @@
 import pytest
 import numpy as np
 import nnabla.functions as F
+import refs
 from nbla_test_utils import list_context
 
 ctxs = list_context('Unpooling')
 
 
-def ref_unpooling(x, kernel):
+def ref_unpooling(x, kernel, channel_last):
+    if channel_last:
+        n_sdim = len(kernel)
+        t = refs.ChannelLastToFirstTranspose(x.ndim, n_sdim)
+        x = t(x)
     y = x
     for ind, p in enumerate(kernel[::-1]):
         y = y.repeat(p, axis=y.ndim - (ind + 1))
+    if channel_last:
+        y = t.inv(y)
     return y
 
 
@@ -31,14 +38,21 @@ def ref_unpooling(x, kernel):
 @pytest.mark.parametrize("seed", [313])
 @pytest.mark.parametrize("inshape", [(1, 2, 3), (2, 4, 6), (2, 2, 4, 6), (2, 2, 2, 4, 6)])
 @pytest.mark.parametrize("kernel", [(1, 1), (2, 3), (2, 1, 2)])
-def test_unpooling_forward_backward(seed, inshape, kernel, ctx, func_name):
+@pytest.mark.parametrize("channel_last", [False, True])
+def test_unpooling_forward_backward(seed, inshape, channel_last, kernel, ctx, func_name):
+    if channel_last and func_name == "Unpooling":
+        pytest.skip("Unpooling with channel_last is only supported in CUDA.")
+    if channel_last and len(inshape) == len(kernel):
+        pytest.skip(
+            "len(input shape) == len(kernel) is only valid for the channel first.")
+
     from nbla_test_utils import function_tester
     rng = np.random.RandomState(seed)
     inputs = [rng.randn(*inshape).astype(np.float32)]
     function_tester(rng,
                     F.unpooling, ref_unpooling,
-                    inputs=inputs,
-                    func_args=[kernel],
+                    inputs=inputs, backward=[True],
+                    func_args=[kernel, channel_last],
                     ctx=ctx, func_name=func_name,
                     atol_f=1e-6, atol_b=1e-2)
 
