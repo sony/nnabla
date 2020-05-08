74acf4dd5028d491f5b5413d039f8c22aa59fbca
Takuya Narihira <Takuya.Narihira@sony.com>
2020-05-08 11:46:47 +0900

    suppress warning due to uninitialized NdArray

--- python/src/nnabla/_nd_array.pyx
+++ python/src/nnabla/_nd_array.pyx
@@ -87,11 +87,11 @@ cdef c_as_numpy_array(CNdArray * arrp, str mode, cpp_bool force_dtype=False, int
             type_num = <int > arrp.array().get().dtype()
         except:
             from nnabla import logger
-            logger.info('Calling the `.data` getter property in this `NdArray` '
-                        'is creating an array with a default data type because '
-                        'the array is not previously initialized by requesting '
-                        'a specific data type, or is not used after '
-                        'calling `zero()` or `fill(value)`.')
+            logger.debug('Calling the `.data` getter property in this `NdArray` '
+                         'is creating an array with a default data type because '
+                         'the array is not previously initialized by requesting '
+                         'a specific data type, or is not used after '
+                         'calling `zero()` or `fill(value)`.')
 
     # Create numpy shape array
     shape.resize(arrp.ndim())
