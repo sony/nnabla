24707c4575232f56e145f507aab4a65843ca4f23
TE-Kazuki.Yoshiyama <Kazuki.Yoshiyama@sony.com>
2020-07-28 18:57:28 +0200

    [Python:grad] Higher-order supports
    [Python:grad] Address multiple grad_outputs with different shapes
    
    [Python:grad] Ignore None in grad_outputs
    
    [Python:grad] Return zero variable value when wrt inputs are unreachable
    
    [Python:grad] Use the corresponding backward method
    
    [Format] nn.grad
    
    [Python:grad] add and comment for the accumulation of grads.
    
    [Python:grad] Rename backward graph to gradient graph
    
    [Python:grad] None check for grad_output
    
    [Python:grad] address floating variable in nn.grad
    
    [Python] not use iadd
    
    [Python:grad] Higher-order supports
    
    [Python:grad] Higher-order supports
    
    [Python:grad] Higher-order supports

--- python/src/nnabla/grad.py
+++ python/src/nnabla/grad.py
@@ -16,9 +16,9 @@ import numpy as np
 from collections import OrderedDict
 
 import nnabla as nn
+import nnabla.functions as F
 from nnabla.function import PythonFunction
-
-from .backward_functions import mappings
+from .backward_functions import registry
 
 
 class GradEndFunction(PythonFunction):
@@ -27,7 +27,7 @@ class GradEndFunction(PythonFunction):
 
     @property
     def name(self):
-        return 'GradEndFunction'
+        return self.__class__.__name__
 
     def min_outputs(self):
         return 1
@@ -57,55 +57,53 @@ class Grad(object):
         else:
             return [x]
 
-    def _connect_on_backward_graph(self, grad_vars, f):
+    def _connect_on_gradient_graph(self, grad_vars, f):
         # 1. accumulate variables used more than one or do nothing
         vf_vb_map = grad_vars.pop(f)  # {VO_fwd: [VI_bwd]}
         grad_inputs = []
         for o in f.outputs:
-            v = vf_vb_map[o]
+            # address `floating` variables; no function takes it as input.
+            # e.g., when dx, db, dg = dBN(...), (db, dg) are not used afterwards.
+            #v = vf_vb_map[o] if o in vf_vb_map else [None]
+            v = vf_vb_map[o] if o in vf_vb_map else [0]
             if len(v) > 1:
                 grad_inputs += [sum(v)]
+                #grad_inputs += [F.add_n(v)]
             else:
                 grad_inputs += v
 
-        # 2. check if grad depends on output data
-        f_outputs = []
-        for i in range(len(f.inputs)):
-            for j in range(len(f.outputs)):
-                if f.grad_depends_output_data(i, j):
-                    # overhead for putting all outputs, but there is no way
-                    f_outputs += f.outputs
-                    break
-        # 3. instantiate backward class
+        # 2. lookup the backward function
         f_fwd_name = f.info.type_name
-        if f_fwd_name not in mappings:
+        if f_fwd_name not in registry:
             raise ValueError(
-                "{} is not in the backward function mappings".format(f_fwd_name))
-        f_bwd_class = mappings[f_fwd_name]
-        f_bwd = f_bwd_class(f.context)
-        f_bwd.set_num_inputs_and_outputs(len(f.inputs) + len(f_outputs) + len(grad_inputs),
-                                         len(f.inputs),
-                                         len(f.inputs),
-                                         len(f.outputs))
-        f_bwd.set_forward_function(f)
+                "{} is not in the backward function registry".format(f_fwd_name))
+        backward_func = registry[f_fwd_name]
 
-        # 4. connect
-        grad_inputs = f.inputs + f_outputs + grad_inputs
-        grad_outputs = f_bwd(*grad_inputs)
+        # 3. connect
+        grad_inputs = grad_inputs + f.inputs
+        ctx = nn.get_current_context()
+        with nn.context_scope(ctx):
+            grad_outputs = backward_func(grad_inputs, **f.info.args)
         grad_outputs = self._force_list(grad_outputs)
 
-        # 5. put each grad_output as grad_input to a corresponding forward function
+        # 4. put grad_output as grad_input to a corresponding function
         for inp, grad_out in zip(f.inputs, grad_outputs):
+            if grad_out is None:
+                continue
             if inp.parent not in grad_vars:
                 grad_vars[inp.parent] = OrderedDict()
             if inp not in grad_vars[inp.parent]:
                 grad_vars[inp.parent][inp] = [grad_out]
             else:
                 grad_vars[inp.parent][inp] += [grad_out]
-
         return grad_outputs
 
-    def _get_corresponding_vars_on_graph(self, inputs, output):
+    def _get_corresponding_vars_on_graph(self, inputs, outputs):
+        """
+        This methods binds the inputs which may be disconnected from a computation graph 
+        (e.g., trainable parameters obtrained by nn.get_parmaeters()) to actual inputs connected 
+        to a given computation graph traversabled from outputs
+        """
         ret = [None for _ in range(len(inputs))]
         inp2id = {x: i for i, x in enumerate(inputs)}
 
@@ -126,11 +124,15 @@ class Grad(object):
                 dfs(par_var)
                 open.add(par_var)
 
-        dfs(output)
+        for output in outputs:
+            dfs(output)
 
         return ret
 
     def _get_children(self, wrt_inputs):
+        """
+        Return children traversable from `wrt_inputs` in the forward graph topological order.
+        """
         children = set()
 
         def dfs(node):
@@ -138,6 +140,8 @@ class Grad(object):
                 return
 
             for f in node.function_references:
+                if f.info.type_name == "GradEndFunction":
+                    continue
                 for o in f.outputs:
                     if o in children:
                         continue
@@ -154,12 +158,9 @@ class Grad(object):
         """
         The logic of this method is almost same as one in visit_function_backward in C++ layer.
         """
-        # TODO: address test in the dynamic graph mode
-        # TODO: address inplace-Function and its test
         # TODO: address auto_forward is very slow. It may be python overhead since small diff when BS is large.
         # TODO: address auto_forward consumes lots of memory, need to call v.get_unlinked_variable()?
         # TODO: address auto_forward consumes lots of memory, need to use NdArray as inputs?
-        # TODO: address `set default context`
 
         # Check outputs/inputs
         outputs = self._force_list(outputs)
@@ -181,17 +182,17 @@ class Grad(object):
             for i in range(len(outputs)):
                 o = outputs[i]
                 go = grad_outputs[i]
-                if not isinstance(go, (type(None), int, float, np.ndarray, nn.NdArray)):
+                if not isinstance(go, (type(None), int, float, np.ndarray, nn.NdArray, nn.Variable)):
                     raise ValueError("Element of `grad_outputs` must be "
-                                     "in (`None`, `int`, `float`, `numpy.ndarray`, `nnabla.NdArray`) or "
-                                     "list of (`None`, `int`, `float`, `numpy.ndarray`, `nnabla.NdArray`)\n"
+                                     "in (`None`, `int`, `float`, `numpy.ndarray`, "
+                                     "`nnabla.NdArray`, `nnabla.Variable`) or "
+                                     "list of (`None`, `int`, `float`, `numpy.ndarray`, "
+                                     "`nnabla.NdArray`, `nnabla.Variable`)\n"
                                      "type(grad_outputs[{}] = {}".format(i, type(go)))
-                elif isinstance(go, np.ndarray) and go.shape != o.shape:
+                elif isinstance(go, (np.ndarray, nn.NdArray, nn.Variable)) and go.shape != o.shape:
                     raise ValueError("Shape of each of outputs and grad_outputs must be same.\n"
                                      "output[{}]({}) != grad_output[{}]({})".format(i, o.shape, i, go.shape))
-                elif isinstance(go, nn.NdArray) and go.shape != o.shape:
-                    raise ValueError("Shape of each of outputs and grad_outputs must be same.\n"
-                                     "output[{}]({}) != grad_output[{}]({})".format(i, o.shape, i, go.shape))
+
         # Check persistent_outputs
         if len(persistent_outputs) != 0 and len(outputs) != len(persistent_outputs):
             raise ValueError("Length of outputs and persistent_outputs "
@@ -204,29 +205,6 @@ class Grad(object):
         for o, p in zip(outputs, persistent_outputs):
             o.persistent = p
 
-        # Set grad_outputs
-        for i in range(len(outputs)):
-            o = outputs[i]
-            go = grad_outputs[i]
-            if go is None:
-                pass
-            elif isinstance(go, (int, float)):
-                grad_output = nn.Variable(o.shape).apply(d=go, need_grad=False)
-                outputs[i] = o * grad_output
-            elif isinstance(go, np.ndarray):
-                grad_output = nn.Variable(o.shape).apply(d=go, need_grad=False)
-                outputs[i] = o * grad_output
-            elif isinstance(go, nn.NdArray):
-                grad_output = nn.Variable(o.shape).apply(
-                    data=go, need_grad=False)
-                outputs[i] = o * grad_output
-
-        # Coerce to sum if there is multiple outputs
-        output = sum(outputs) if len(outputs) != 1 else outputs[0]
-
-        # Connect the forward and backward graph
-        grad_output = GradEndFunction()(output).apply(need_grad=False)
-
         # Open list of next search candidate
         ids = {}
 
@@ -237,53 +215,80 @@ class Grad(object):
                 return size
             return ids[func]
         open = set()
-        func = output.parent
-        open.add((-output.rank, get_id(func), func))
 
-        # Map for grad_variables consumed on the backward graph
+        # Map for grad_variables consumed on the gradient graph.
+        # End is the special case where d_o = end_f(o) and map[end_f] = {o: [d_o]}
         grad_vars = OrderedDict()  # {F_fwd: {VO_fwd: [VI_bwd]}}
-        grad_vars[func] = OrderedDict({output: [grad_output]})
 
-        # Return grads
-        # replace inputs params with the vars connected with given graph
-        wrt_inputs = self._get_corresponding_vars_on_graph(inputs, output)
+        # Set grad_outputs
+        for i in range(len(outputs)):
+            o = outputs[i]
+            go = grad_outputs[i]
+            if go is None:
+                output = o
+            elif isinstance(go, (int, float)):
+                go = nn.Variable(o.shape).apply(d=go, need_grad=False)
+                output = o * go
+            elif isinstance(go, np.ndarray):
+                go = nn.Variable(o.shape).apply(d=go, need_grad=False)
+                output = o * go
+            elif isinstance(go, nn.NdArray):
+                go = nn.Variable(o.shape).apply(data=go, need_grad=False)
+                output = o * go
+            elif isinstance(go, nn.Variable):
+                output = o * go
+            func = output.parent
+            open.add((-output.rank, get_id(func), func))
+
+            # Connect the graph and its gradient graph
+            grad_output = GradEndFunction()(output).apply(need_grad=False)
+            grad_vars[func] = OrderedDict({output: [grad_output]})
+
+        # Return grads but
+        # replace inputs params with the vars connected with the given graph
+        wrt_inputs = self._get_corresponding_vars_on_graph(inputs, outputs)
         grads = [None] * len(wrt_inputs)
-
         child_nodes = self._get_children(wrt_inputs)
 
-        # Expand the forward graph to the backward graph
+        # Expand the graph to its gradient graph
         while len(open) != 0:
             open = sorted(open)  # python set is NOT sorted set.
             rank_func = open.pop(0)  # 0 is necessary
             open = set(open)
             f = rank_func[2]
+
             if not f.need_grad:
                 continue
-            # Connect variables on the backward graph
-            grad_outputs = self._connect_on_backward_graph(grad_vars, f)
+            # Connect variables on the gradient graph
+            grad_outputs = self._connect_on_gradient_graph(grad_vars, f)
 
             # Check grads w.r.t. inputs
             for inp, grad_out in zip(f.inputs, grad_outputs):
-                if inp not in wrt_inputs or inp.need_grad == False:
+                if inp not in wrt_inputs or inp.need_grad == False or grad_out is None:
                     continue
                 idx = wrt_inputs.index(inp)
                 if grads[idx] is None:
                     grads[idx] = grad_out
                 else:
-                    grads[idx] += grad_out  # accum at leaf
+                    grads[idx] = grads[idx] + grad_out  # accum at leaf
                 if bind_grad_output:
                     inp.grad = grads[idx].data
 
             # Propagate down
-            for inp in f.inputs:
-                if inp not in child_nodes or not inp.need_grad:
+            for inp, grad_out in zip(f.inputs, grad_outputs):
+                # if inp not in child_nodes and not inp.need_grad or grad_out is None:
+                if inp not in child_nodes and not inp.need_grad or grad_out is None:
                     continue
-
                 p_i = inp.parent
                 if not p_i:
                     continue
                 open.add((-p_i.rank, get_id(p_i), p_i))
 
+        # If the final grads has None, then None becomes zero Variable(s).
+        for i in range(len(grads)):
+            if grads[i]:
+                continue
+            grads[i] = F.constant(0, wrt_inputs[i].shape)
         return grads
 
 
@@ -301,16 +306,17 @@ def grad(outputs, inputs, grad_outputs=None, persistent_outputs=[], bind_grad_ou
     Args: 
         outputs (list of :obj:`~nnabla.Variable` or :obj:`~nnabla.Variable`): Outputs of the differentiable function.
         inputs (list of :obj:`~nnabla.Variable` or :obj:`~nnabla.Variable`): Inputs w.r.t. which the gradients of outputs are computed.
-        grad_outputs (None, scalar, :obj:`numpy.ndarray`, :obj:`nnabla.NdArray`, or list of scalar, :obj:`numpy.ndarray`, or :obj:`nnabla.NdArray`, ): Gradient outputs corresponding to outputs. This is same as the grad argument of :meth:`~nnabla.Variable.backward`. Default is None, so 1 is used as the in-coming gradient at the very beginning of the Variable in the backward graph.
+        grad_outputs (None, scalar, :obj:`numpy.ndarray`, :obj:`nnabla.NdArray`, or list of scalar, :obj:`numpy.ndarray`, or :obj:`nnabla.NdArray`, ): Gradient outputs corresponding to outputs. This is same as the grad argument of :meth:`~nnabla.Variable.backward`. Default is None, so 1 is used as the in-coming gradient at the very beginning of the Variable in the gradient graph.
         persistent_outputs (list of `bool`): Outputs become persistent accordingly. If not specified, all outputs become persistent.
-        bind_grad_output (`bool`): Bind data to grad of input variable. This is useful for the case where one wants to use the backward graph for training a neural network using the first-order gradients only. Default is False.
+        bind_grad_output (`bool`): Bind data to grad of input variable. This is useful for the case where one wants to use the gradient graph for training a neural network using the first-order gradients only. Default is False.
 
     Returns
-        List of :obj:`~nnabla.Variable`s. 
+        List of :obj:`~nnabla.Variable`.
 
-        If the backpropagation does not reach input(s), the corresponding returned value(s) are None.
+        If the backpropagation does not reach input(s), the corresponding returned value(s) are `zero`
+        (i.e., the gradients w.r.t. inputs are zero) and not connected as a part of the gradient graph.
 
-    Example: 
+    Example (Gradient Penalty): 
 
     .. code-block:: python
 
@@ -323,6 +329,7 @@ def grad(outputs, inputs, grad_outputs=None, persistent_outputs=[], bind_grad_ou
         # Context
         extension_module = "cudnn"
         ctx = get_extension_context(extension_module)
+        nn.set_default_context(ctx)
 
         # Input and label
         x = nn.Variable.from_numpy_array(np.random.randn(4, 3, 32, 32))
@@ -343,13 +350,40 @@ def grad(outputs, inputs, grad_outputs=None, persistent_outputs=[], bind_grad_ou
         inputs = nn.get_parameters().values()
         grads = nn.grad(outputs, inputs)  # gradients of the parameters
 
-        # Double backward of the outputs w.r.t. the parameters by constraining the gradient norms.
-        gp = sum([F.sum(g ** 2.0) ** 0.5 for g in grads])
+        # Backward of the outputs w.r.t. the parameters by constraining the gradient norms.
+        t = 0 # or 1
+        gp = sum([(F.sum(g ** 2) ** 0.5 - t) ** 2 for g in grads])
         loss += gp
         loss.forward()
         loss.backward()
 
-    """
+
+    Example (Higer-order Gradients): 
+
+    .. code-block:: python
+
+        import nnabla as nn
+        import nnabla.functions as F
+        import numpy as np
+
+        x = nn.Variable.from_numpy_array(np.random.randn(2, 2)).apply(need_grad=True)
+        x.grad.zero()
+        y = F.sin(x)
+        def grad(y, x, n=1):
+            dx = [y]
+            for _ in range(n):
+                dx = nn.grad([dx[0]], [x])
+            return dx[0]
+        dnx = grad(y, x, n=10)
+        dnx.forward()
+        print(np.allclose(-np.sin(x.d), dnx.d))
+        dnx.backward()
+        print(np.allclose(-np.cos(x.d), x.g))
+
+        # Show the supported status for each function
+        from nnabla.backward_functions import show_registry
+        show_registry()
+        """
 
     grad_outputs = Grad()(outputs, inputs, grad_outputs=grad_outputs,
                           persistent_outputs=persistent_outputs,
