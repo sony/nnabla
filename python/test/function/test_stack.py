3d3e2bcde221559e5e98e9037549a0739ca2953f
TE-andrewshin <33474392+TE-andrewshin@users.noreply.github.com>
2021-03-01 12:56:10 +0900

    Merge pull request #813 from sony/fix/20210225-fix-stack-negative-axis
    Fix negative axis for stack function

--- python/test/function/test_stack.py
+++ python/test/function/test_stack.py
@@ -26,7 +26,7 @@ def ref_stack(*inputs, **params):
 
 
 @pytest.mark.parametrize("ctx, func_name", ctxs)
-@pytest.mark.parametrize("axis", [0, 1, 2])
+@pytest.mark.parametrize("axis", [0, 1, 2, -1, -2, -3])
 @pytest.mark.parametrize("seed", [313])
 @pytest.mark.parametrize("num_inputs", [2, 3])
 def test_stack_forward_backward(seed, axis, num_inputs, ctx, func_name):
@@ -40,7 +40,7 @@ def test_stack_forward_backward(seed, axis, num_inputs, ctx, func_name):
 
 
 @pytest.mark.parametrize("ctx, func_name", ctxs)
-@pytest.mark.parametrize("axis", [0, 1, 2])
+@pytest.mark.parametrize("axis", [0, 1, 2, -1, -2, -3])
 @pytest.mark.parametrize("seed", [313])
 @pytest.mark.parametrize("num_inputs", [2, 3])
 def test_stack_double_backward(seed, axis, num_inputs, ctx, func_name):
