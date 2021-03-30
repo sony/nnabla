cbfda7484ff46465fee69d6766a682a50fbe3fe7
NNabla developer <nnabla@example.com>
2021-03-31 01:26:52 +0900

    Binary operator and broadcast were fused.

--- include/nbla/function/utils/base_transform_binary.hpp
+++ include/nbla/function/utils/base_transform_binary.hpp
@@ -17,11 +17,11 @@
 #ifndef __NBLA_FUNCTION_BASE_TRANSFORM_BINARY_HPP__
 #define __NBLA_FUNCTION_BASE_TRANSFORM_BINARY_HPP__
 
+#include <nbla/common.hpp>
 #include <nbla/cpu.hpp>
 #include <nbla/function.hpp>
-#include <nbla/function/broadcast.hpp>
 #include <nbla/function_registry.hpp>
-#include <nbla/imperative.hpp>
+#include <nbla/half.hpp>
 
 namespace nbla {
 
@@ -31,8 +31,13 @@ namespace nbla {
 template <typename... Args>
 class BaseTransformBinary : public BaseFunction<Args...> {
 protected:
-  shared_ptr<Function> f_bc0_, f_bc1_;
-  bool inplace_;
+  const bool inplace_;
+
+  Size_t compressed_ndim_;
+  Variable strides_x0_;
+  Variable strides_x1_;
+  Variable strides_y_;
+  Variable shape_y_;
 
 public:
   BaseTransformBinary(const Context &ctx, bool inplace, Args... args)
@@ -52,7 +57,7 @@ public:
 
 protected:
   virtual void setup_impl(const Variables &inputs, const Variables &outputs) {
-    int ndim = inputs[0]->ndim();
+    Size_t ndim = inputs[0]->ndim();
     NBLA_CHECK(ndim == inputs[1]->ndim(), error_code::value,
                "Dimensions of inputs must match. "
                "inputs[0]: %d != inputs[1]: %d.",
@@ -60,19 +65,12 @@ protected:
     Shape_t s0 = inputs[0]->shape();
     Shape_t s1 = inputs[1]->shape();
     Shape_t oshape(ndim);
-    bool bc0 = false;
-    bool bc1 = false;
-    for (int i = 0; i < ndim; ++i) {
+
+    for (Size_t i = 0; i < ndim; ++i) {
       if (s0[i] != s1[i]) {
         NBLA_CHECK(std::min(s0[i], s1[i]) == 1, error_code::value,
                    "Broadcast dimension must be one. shape[%d]: %d.", i,
                    std::min(s0[i], s1[i]));
-        if (s0[i] == 1) {
-          bc0 = true;
-        }
-        if (s1[i] == 1) {
-          bc1 = true;
-        }
       }
       oshape[i] = std::max(s0[i], s1[i]);
     }
@@ -87,13 +85,131 @@ protected:
       outputs[0]->data()->set_array(inputs[0]->data()->array());
     }
 
-    if (bc0) {
-      f_bc0_ = create_Broadcast(this->ctx_,
-                                vector<int>(oshape.cbegin(), oshape.cend()));
+    // [ Stride computation ]
+    // Broadcast for x0 and x1 are performed only by using their strides
+    // while computing binary operation. For computational efficiency
+    // in cuda backend, the dimensional compression into three are performed.
+    // Let [z, y, x] the shape of compressed three dimensions. x-axis is
+    // the innermost dimension in NNabla. If compression get success,
+    // the final shape must become one of the following five patterns.
+    //
+    //       no broadcast    broadcast    broadcast          broadcast
+    //                        y-axis       x-axis         (x and z)-axis
+    //
+    //  x0    [1, 1, x]      [z, y, x]    [1, y, x]    [z, y, x]  [1, y, x]
+    //  x1    [1, 1, x]      [z, 1, x]    [1, y, 1]    [1, y, 1]  [z, y, 1]
+    //
+    // Otherwise the compressed dimenstion is more than four. Of cource,
+    // the shapes of x0 and x1 are exchangable.
+
+    // Compress the adjascent dimensions which are broadcasted in the same way.
+    // In detail, they can be compressed in these three cases.
+    //
+    //      no broadcast   x0 broadcasted   x1 broadcasted
+    //
+    // s0      [a, b]          [1, 1]           [a, b]
+    // s1      [a, b]          [a, b]           [1, 1]
+    //
+    Shape_t compressed_shape_x0;
+    Shape_t compressed_shape_x1;
+    Shape_t compressed_shape_y;
+    Size_t tmp_size_x0 = 1;
+    Size_t tmp_size_x1 = 1;
+    Size_t tmp_size_y = 1;
+
+    for (Size_t i = 0; i < ndim - 1; ++i) {
+      tmp_size_x0 *= s0[i];
+      tmp_size_x1 *= s1[i];
+      tmp_size_y *= oshape[i];
+
+      // Stop this adjascent compression when i- amd (i + 1)-th dimensions
+      // do not share the same case among the above three cases.
+      // continue above three cases.
+      // i-th case
+      const bool no_broadcast = (s0[i] == s1[i]);
+      const bool x0_broadcast = (s0[i] == 1 && s1[i] != 1);
+      const bool x1_broadcast = (s0[i] != 1 && s1[i] == 1);
+      // (i + 1)-th case
+      const bool no_broadcast_next = (s0[i + 1] == s1[i + 1]);
+      const bool x0_broadcast_next = (s0[i + 1] == 1 && s1[i + 1] != 1);
+      const bool x1_broadcast_next = (s0[i + 1] != 1 && s1[i + 1] == 1);
+
+      if (!(no_broadcast && no_broadcast_next) &&
+          !(x0_broadcast && x0_broadcast_next) &&
+          !(x1_broadcast && x1_broadcast_next)) {
+        // Stop the adjascent compression
+        compressed_shape_x0.push_back(tmp_size_x0);
+        compressed_shape_x1.push_back(tmp_size_x1);
+        compressed_shape_y.push_back(tmp_size_y);
+        tmp_size_x0 = 1;
+        tmp_size_x1 = 1;
+        tmp_size_y = 1;
+      }
     }
-    if (bc1) {
-      f_bc1_ = create_Broadcast(this->ctx_,
-                                vector<int>(oshape.cbegin(), oshape.cend()));
+    if (ndim > 0) {
+      tmp_size_x0 *= s0[ndim - 1];
+      tmp_size_x1 *= s1[ndim - 1];
+      tmp_size_y *= oshape[ndim - 1];
+      compressed_shape_x0.push_back(tmp_size_x0);
+      compressed_shape_x1.push_back(tmp_size_x1);
+      compressed_shape_y.push_back(tmp_size_y);
+    }
+
+    // if the number of the compressed dimensions are less than three,
+    // pad it up to three.
+    while (compressed_shape_y.size() < 3) {
+      compressed_shape_x0.insert(compressed_shape_x0.begin(), 1);
+      compressed_shape_x1.insert(compressed_shape_x1.begin(), 1);
+      compressed_shape_y.insert(compressed_shape_y.begin(), 1);
+    }
+    compressed_ndim_ = compressed_shape_y.size();
+
+    // Compress their strides
+    Shape_t compressed_strides_x0(compressed_ndim_, 1);
+    Shape_t compressed_strides_x1(compressed_ndim_, 1);
+    Shape_t compressed_strides_y(compressed_ndim_, 1);
+
+    for (Size_t i = compressed_ndim_ - 2; i >= 0; --i) {
+      compressed_strides_x0[i] =
+          compressed_strides_x0[i + 1] * compressed_shape_x0[i + 1];
+      compressed_strides_x1[i] =
+          compressed_strides_x1[i + 1] * compressed_shape_x1[i + 1];
+      compressed_strides_y[i] =
+          compressed_strides_y[i + 1] * compressed_shape_y[i + 1];
+    }
+
+    // Store the compressed strides and shape as Variable.
+    // To broadcast an axis, 0 must be set to its stride instead of 1.
+    strides_x0_.reshape({compressed_ndim_}, true);
+    strides_x1_.reshape({compressed_ndim_}, true);
+    strides_y_.reshape({compressed_ndim_}, true);
+    shape_y_.reshape({compressed_ndim_}, true);
+
+    Context cpu_ctx = Context().set_array_class(
+        SingletonManager::get<Cpu>()->array_classes()[0]);
+    Size_t *strides_x0 =
+        strides_x0_.cast_data_and_get_pointer<Size_t>(cpu_ctx, true);
+    Size_t *strides_x1 =
+        strides_x1_.cast_data_and_get_pointer<Size_t>(cpu_ctx, true);
+    Size_t *strides_y =
+        strides_y_.cast_data_and_get_pointer<Size_t>(cpu_ctx, true);
+    Size_t *shape_y = shape_y_.cast_data_and_get_pointer<Size_t>(cpu_ctx, true);
+
+    for (Size_t i = 0; i < compressed_ndim_; ++i) {
+      shape_y[i] = compressed_shape_y[i];
+      strides_y[i] = compressed_strides_y[i];
+
+      if (compressed_shape_x0[i] == compressed_shape_y[i]) {
+        strides_x0[i] = compressed_strides_x0[i];
+      } else {
+        strides_x0[i] = 0;
+      }
+
+      if (compressed_shape_x1[i] == compressed_shape_y[i]) {
+        strides_x1[i] = compressed_strides_x1[i];
+      } else {
+        strides_x1[i] = 0;
+      }
     }
   }
 };
@@ -145,48 +261,97 @@ public:
 };
 
 template <typename T, typename BinaryOp>
-void transform_binary(int size, const T *x0, const T *x1, T *y, BinaryOp op) {
-  for (int idx = 0; idx < size; ++idx) {
-    y[idx] = op(x0[idx], x1[idx]);
+void transform_binary(const Size_t size, const T *x0, const T *x1, T *y,
+                      BinaryOp op, const Size_t ndim, const Size_t *strides_x0,
+                      const Size_t *strides_x1, const Size_t *strides_y,
+                      const Size_t *shape_y) {
+  // Convert the type of intermidiate buffers from Half to float to suppress
+  // a decrease in precision during computation.
+  using PRECISE_T = typename force_float<T>::type;
+
+  for (Size_t idx = 0; idx < size; ++idx) {
+    Size_t idx0 = 0;
+    Size_t idx1 = 0;
+    for (Size_t i = 0; i < ndim; ++i) {
+      Size_t dim_idx = (idx / strides_y[i]) % shape_y[i];
+      idx0 += dim_idx * strides_x0[i];
+      idx1 += dim_idx * strides_x1[i];
+    }
+    y[idx] =
+        op(static_cast<PRECISE_T>(x0[idx0]), static_cast<PRECISE_T>(x1[idx1]));
   }
 }
 
-template <typename T, typename BinaryOp, bool accum>
-void transform_binary_grad0(int size, const T *dy, const T *x0, const T *x1,
-                            const T *y, T *g0, bool inplace, BinaryOp op) {
-  for (int idx = 0; idx < size; ++idx) {
-    g0[idx] = (accum ? g0[idx] : (T)0) +
-              op.g0(dy[idx], x0[idx], x1[idx], y[idx], inplace);
+template <typename T, typename BinaryOp>
+void transform_binary_grad0(const Size_t size, const T *dy, const T *x0,
+                            const T *x1, const T *y, T *g0, const bool inplace,
+                            BinaryOp op, const Size_t ndim,
+                            const Size_t *strides_x0, const Size_t *strides_x1,
+                            const Size_t *strides_y, const Size_t *shape_y) {
+  // Convert the type of intermidiate buffers from Half to float to suppress
+  // a decrease in precision during computation.
+  using PRECISE_T = typename force_float<T>::type;
+
+  for (Size_t idx = 0; idx < size; ++idx) {
+    Size_t idx0 = 0;
+    Size_t idx1 = 0;
+    for (Size_t i = 0; i < ndim; ++i) {
+      Size_t dim_idx = (idx / strides_y[i]) % shape_y[i];
+      idx0 += dim_idx * strides_x0[i];
+      idx1 += dim_idx * strides_x1[i];
+    }
+    g0[idx0] =
+        static_cast<PRECISE_T>(g0[idx0]) +
+        op.g0(static_cast<PRECISE_T>(dy[idx]), static_cast<PRECISE_T>(x0[idx0]),
+              static_cast<PRECISE_T>(x1[idx1]), static_cast<PRECISE_T>(y[idx]),
+              inplace);
   }
 }
 
-template <typename T, typename BinaryOp, bool accum>
-void transform_binary_grad1(int size, const T *dy, const T *x0, const T *x1,
-                            const T *y, T *g1, bool inplace, BinaryOp op) {
-  for (int idx = 0; idx < size; ++idx) {
-    g1[idx] = (accum ? g1[idx] : (T)0) +
-              op.g1(dy[idx], x0[idx], x1[idx], y[idx], inplace);
+template <typename T, typename BinaryOp>
+void transform_binary_grad1(const Size_t size, const T *dy, const T *x0,
+                            const T *x1, const T *y, T *g1, const bool inplace,
+                            BinaryOp op, const Size_t ndim,
+                            const Size_t *strides_x0, const Size_t *strides_x1,
+                            const Size_t *strides_y, const Size_t *shape_y) {
+  // Convert the type of intermidiate buffers from Half to float to suppress
+  // a decrease in precision during computation.
+  using PRECISE_T = typename force_float<T>::type;
+
+  for (Size_t idx = 0; idx < size; ++idx) {
+    Size_t idx0 = 0;
+    Size_t idx1 = 0;
+    for (Size_t i = 0; i < ndim; ++i) {
+      Size_t dim_idx = (idx / strides_y[i]) % shape_y[i];
+      idx0 += dim_idx * strides_x0[i];
+      idx1 += dim_idx * strides_x1[i];
+    }
+    g1[idx1] =
+        static_cast<PRECISE_T>(g1[idx1]) +
+        op.g1(static_cast<PRECISE_T>(dy[idx]), static_cast<PRECISE_T>(x0[idx0]),
+              static_cast<PRECISE_T>(x1[idx1]), static_cast<PRECISE_T>(y[idx]),
+              inplace);
   }
 }
 
 template <typename T, typename BinaryOp, typename... Args>
 void TransformBinary<T, BinaryOp, Args...>::forward_impl(
     const Variables &inputs, const Variables &outputs) {
-  auto _get = [this](Variable *v) {
-    return v->get_data_pointer<T>(this->ctx_);
-  };
-  // Broadcast
-  Variable o_bc0;
-  Variable o_bc1;
-  if (this->f_bc0_)
-    execute(this->f_bc0_, {inputs[0]}, {&o_bc0});
-  if (this->f_bc1_)
-    execute(this->f_bc1_, {inputs[1]}, {&o_bc1});
-  // Binary transform
-  const T *x0 = _get((this->f_bc0_) ? (&o_bc0) : (inputs[0]));
-  const T *x1 = _get((this->f_bc1_) ? (&o_bc1) : (inputs[1]));
+  const T *x0 = inputs[0]->get_data_pointer<T>(this->ctx_);
+  const T *x1 = inputs[1]->get_data_pointer<T>(this->ctx_);
   T *y = outputs[0]->cast_data_and_get_pointer<T>(this->ctx_, !this->inplace_);
-  transform_binary(outputs[0]->size(), x0, x1, y, binary_op_);
+  const Size_t *strides_x0 =
+      this->strides_x0_.template get_data_pointer<Size_t>(this->ctx_);
+  const Size_t *strides_x1 =
+      this->strides_x1_.template get_data_pointer<Size_t>(this->ctx_);
+  const Size_t *strides_y =
+      this->strides_y_.template get_data_pointer<Size_t>(this->ctx_);
+  const Size_t *shape_y =
+      this->shape_y_.template get_data_pointer<Size_t>(this->ctx_);
+
+  transform_binary(outputs[0]->size(), x0, x1, y, binary_op_,
+                   this->compressed_ndim_, strides_x0, strides_x1, strides_y,
+                   shape_y);
 }
 
 template <typename T, typename BinaryOp, typename... Args>
@@ -196,62 +361,38 @@ void TransformBinary<T, BinaryOp, Args...>::backward_impl(
   if (!(propagate_down[0] || propagate_down[1])) {
     return;
   }
-  auto _get_data = [this](Variable *v) {
-    return v->get_data_pointer<T>(this->ctx_);
-  };
-  auto _cast_grad = [this](Variable *v, bool wo) {
-    return v->cast_grad_and_get_pointer<T>(this->ctx_, wo);
-  };
+
+  const T *x0 = inputs[0]->get_data_pointer<T>(this->ctx_);
+  const T *x1 = inputs[1]->get_data_pointer<T>(this->ctx_);
+  const T *y = outputs[0]->get_data_pointer<T>(this->ctx_);
   const T *dy = outputs[0]->get_grad_pointer<T>(this->ctx_);
-  const T *y = _get_data(outputs[0]);
+  const Size_t *strides_x0 =
+      this->strides_x0_.template get_data_pointer<Size_t>(this->ctx_);
+  const Size_t *strides_x1 =
+      this->strides_x1_.template get_data_pointer<Size_t>(this->ctx_);
+  const Size_t *strides_y =
+      this->strides_y_.template get_data_pointer<Size_t>(this->ctx_);
+  const Size_t *shape_y =
+      this->shape_y_.template get_data_pointer<Size_t>(this->ctx_);
   Size_t size = outputs[0]->size();
+
   if (propagate_down[0]) {
-    // Broadcast
-    Variable o_bc0;
-    Variable o_bc1;
-    if (this->f_bc0_)
-      execute(this->f_bc0_, {inputs[0]}, {&o_bc0});
-    if (this->f_bc1_)
-      execute(this->f_bc1_, {inputs[1]}, {&o_bc1});
-    // Binary transform backward
-    const T *x0 = _get_data((this->f_bc0_) ? (&o_bc0) : (inputs[0]));
-    const T *x1 = _get_data((this->f_bc1_) ? (&o_bc1) : (inputs[1]));
-    T *dx0 = (this->f_bc0_) ? _cast_grad(&o_bc0, true)
-                            : _cast_grad(inputs[0], !accum[0]);
-    if ((!this->f_bc0_) && accum[0])
-      transform_binary_grad0<T, BinaryOp, true>(size, dy, x0, x1, y, dx0,
-                                                this->inplace_, binary_op_);
-    else
-      transform_binary_grad0<T, BinaryOp, false>(size, dy, x0, x1, y, dx0,
-                                                 this->inplace_, binary_op_);
-    // Broadcast backward
-    if (this->f_bc0_)
-      nbla::backward(this->f_bc0_, Variables{inputs[0]}, Variables{&o_bc0},
-                     {true}, {accum[0]});
+    if (!accum[0]) {
+      inputs[0]->grad()->zero();
+    }
+    T *dx0 = inputs[0]->cast_grad_and_get_pointer<T>(this->ctx_);
+    transform_binary_grad0<T, BinaryOp>(
+        size, dy, x0, x1, y, dx0, this->inplace_, binary_op_,
+        this->compressed_ndim_, strides_x0, strides_x1, strides_y, shape_y);
   }
   if (propagate_down[1]) {
-    // Broadcast
-    Variable o_bc0;
-    Variable o_bc1;
-    if (this->f_bc0_)
-      execute(this->f_bc0_, {inputs[0]}, {&o_bc0});
-    if (this->f_bc1_)
-      execute(this->f_bc1_, {inputs[1]}, {&o_bc1});
-    // Binary transform backward
-    const T *x0 = _get_data((this->f_bc0_) ? (&o_bc0) : (inputs[0]));
-    const T *x1 = _get_data((this->f_bc1_) ? (&o_bc1) : (inputs[1]));
-    T *dx1 = (this->f_bc1_) ? _cast_grad(&o_bc1, true)
-                            : _cast_grad(inputs[1], !accum[1]);
-    if ((!this->f_bc1_) && accum[1])
-      transform_binary_grad1<T, BinaryOp, true>(size, dy, x0, x1, y, dx1,
-                                                this->inplace_, binary_op_);
-    else
-      transform_binary_grad1<T, BinaryOp, false>(size, dy, x0, x1, y, dx1,
-                                                 this->inplace_, binary_op_);
-    // Broadcast backward
-    if (this->f_bc1_)
-      nbla::backward(this->f_bc1_, Variables{inputs[1]}, Variables{&o_bc1},
-                     {true}, {accum[1]});
+    if (!accum[1]) {
+      inputs[1]->grad()->zero();
+    }
+    T *dx1 = inputs[1]->cast_grad_and_get_pointer<T>(this->ctx_);
+    transform_binary_grad1<T, BinaryOp>(
+        size, dy, x0, x1, y, dx1, this->inplace_, binary_op_,
+        this->compressed_ndim_, strides_x0, strides_x1, strides_y, shape_y);
   }
 }
 
@@ -262,12 +403,14 @@ void TransformBinary<T, BinaryOp, Args...>::backward_impl(
   template <typename T> inline T operator()(const T x0, const T x1) {          \
     return OP;                                                                 \
   }
+
 #define NBLA_DEFINE_BINARY_OP_BACKWARD(NUM, GOP)                               \
   template <typename T>                                                        \
   inline T g##NUM(const T dy, const T x0, const T x1, const T y,               \
                   const bool inplace) {                                        \
     return GOP;                                                                \
   }
+
 #define NBLA_DEFINE_TRANSFORM_BINARY_CLASS_COMMON(NAME, DEP_Y_0, DEP_Y_1)      \
 public:                                                                        \
   virtual ~NAME() {}                                                           \
