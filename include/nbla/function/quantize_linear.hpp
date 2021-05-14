ac3bd245e8f879df342aa7435c65a5b965bf0389
NNabla developer <nnabla@example.com>
2021-05-14 18:29:29 +0900

    add grad_depends_input_data_impl to QuantizeLinear

--- include/nbla/function/quantize_linear.hpp
+++ include/nbla/function/quantize_linear.hpp
@@ -81,6 +81,12 @@ protected:
                                       const Variables &outputs,
                                       const vector<bool> &propagate_down,
                                       const vector<bool> &accum);
+  virtual bool grad_depends_input_data_impl(int i, int j) const {
+    if (i == 0 && j == 1) {
+      return true;
+    }
+    return false;
+  }
   NBLA_API virtual void round(Variable *inp, std::string round_mode);
   NBLA_API virtual void saturate(Variable *inp, int min_range, int max_range);
 };
