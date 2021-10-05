9fadcd407e4dc9457f3f7420ca6dac6a9a086494
NNabla developer <nnabla@example.com>
2021-10-06 00:21:14 +0900

    Separate ChannelFirstAdaptor implementation and add more comments

--- include/nbla/function/utils/channel_first_adaptor.hpp
+++ include/nbla/function/utils/channel_first_adaptor.hpp
@@ -21,104 +21,133 @@
 #include <nbla/function/transpose.hpp>
 
 namespace nbla {
+using std::vector;
+
+/**
+ * @brief This class can be used to transform a variable memory format to
+ * channel-first.
+ *
+ * Typical use case is a transformation from channel-last to channel-first
+ * memory format in convolutional neural network.
+ *
+ * For example, let an input variable shape (32, 128, 128, 3), batch_axis ==
+ * [0] and channel_axis == 3 (channel-last). Then this adaptor convert the
+ * shape and memory format to (32, 3, 128, 128) by applying transpose function.
+ *
+ * Possible use case is a force use of channel-first implementation (layer
+ * functions) in a channel-last network by sandwiching channel-first
+ * implementation with this adaptor. The conceptual work flow (forward prop) is
+ * following.
+ * chennal-last layer -> forward_pre -> channel-first layer -> forward_post ->
+ * channel-last layer
+ * (Variebles are omitted and same for backward prop)
+ */
 class ChannelFirstAdaptor {
   FunctionPtr pre_transpose_, post_transpose_;
 
 public:
-  static bool need_adaptor(const Shape_t &shape,
-                           const std::vector<int> &batch_axis,
-                           const int channel_axis) {
-    // Check batch_axis
-    auto sorted_batch_axis = batch_axis;
-    std::sort(sorted_batch_axis.begin(), sorted_batch_axis.end());
-    for (int i = 0; i < sorted_batch_axis.size(); i++) {
-      if (sorted_batch_axis[i] != i) {
-        return true;
-      }
-    }
+  /**
+   * @brief Check wheather this adaptor needed for the input.
+   *
+   * Returns `false` when the input is already channel-first format, otherwise
+   * `true`.
+   * The definition of channel-first memory format is when all of the following
+   * conditions are met.
+   * * `batch_axis` must be contiguous to the beginning of the shape.
+   * * `channel_axis` must be right after the last axis of `batch_axis`.
+   *
+   * Examples of channel-first
+   * * ndim: 4, batch_axis: [0], channel_axis: 1
+   * * ndim: 2, batch_axis: [0], channel_axis: 1
+   * * ndim: 2, batch_axis: [], channel_axis: 0
+   * * ndim: 1, batch_axis: [], channel_axis: 0
+   * * ndim: 6, batch_axis: [0, 1, 2], channel_axis: 3
+   * * ndim: 6, batch_axis: [1, 0, 2], channel_axis: 3
+   *
+   * Examples of non channel-first
+   * * ndim: 4, batch_axis: [0], channel_axis: 3
+   * * ndim: 4, batch_axis: [0], channel_axis: 2
+   * * ndim: 4, batch_axis: [1], channel_axis: 0
+   * * ndim: 2, batch_axis: [], channel_axis: 1
+   * * ndim: 6, batch_axis: [0, 1, 3], channel_axis: 2
+   *
+   * @param shape Shape of original input.
+   * @param batch_axis List of integer corresponding to batch or outer axis.
+   * Each axis must be unique and in range of [0, ndim).
+   * @param channel_axis An integer corresponding to channel axis. This axis
+   * must be in range of [0, ndim).
+   * @return true
+   * @return false
+   */
+  static bool need_adaptor(const Shape_t &shape, const vector<int> &batch_axis,
+                           const int channel_axis);
 
-    // Check channel_axis
-    if (channel_axis != sorted_batch_axis.size()) {
-      return true;
-    }
-
-    // The input is channel-first memory format.
-    return false;
-  }
-
+  /**
+   * @brief Setup the adaptor.
+   *
+   * This method must be called before the use of `forward_(pre|post)` or
+   * `backward_(pre|post)` methods. Setting up of transpose functions
+   * (`(pre|post)_transpose_`) are performed internally.
+   *
+   * @param input_pre Variable pointer for `pre_transpose_` input.
+   * @param output_pre Variable pointer for `pre_transpose_` output.
+   * @param input_post Variable pointer for `post_transpose_` input.
+   * @param output_post Variable pointer for `post_transpose_` output.
+   * @param shape Shape of original input. This should be same as the shape of
+   * `input_pre`.
+   * @param batch_axis List of integer corresponding to batch or outer axis.
+   * Each axis must be in range of [0, ndim).
+   * @param channel_axis An integer corresponding to channel axis. This axis
+   * must be in range of [0, ndim).
+   * @param ctx A compute backend descriptor.
+   */
   void setup(Variable *input_pre, Variable *output_pre, Variable *input_post,
              Variable *output_post, const Shape_t &shape,
-             const std::vector<int> &batch_axis, const int channel_axis,
-             const Context &ctx) {
-    const int ndim = shape.size();
+             const vector<int> &batch_axis, const int channel_axis,
+             const Context &ctx);
 
-    // Pre-transpose
-    std::vector<int> pre_transpose_shape;
-    for (const auto b : batch_axis) {
-      pre_transpose_shape.push_back(b);
-    }
-    pre_transpose_shape.push_back(channel_axis);
+  /**
+   * @brief Transform variable memory format to channel-first.
+   *
+   * @param input `pre_tranpose_` input.
+   * @param output `pre_tranpose_` output.
+   */
+  void convert_to_channel_first(Variable *input, Variable *output);
 
-    for (int i = 0; i < ndim; i++) {
-      if (i == channel_axis) {
-        continue;
-      }
+  /**
+   * @brief Transform variable memory format from channel-first to original one.
+   *
+   * @param input `post_tranpose_` input.
+   * @param output `post_tranpose_` output.
+   */
+  void convert_from_channel_first(Variable *input, Variable *output);
 
-      // Check if `batch_axis` contains `i`.
-      bool next = false;
-      for (const auto b : batch_axis) {
-        if (i == b) {
-          next = true;
-          break;
-        }
-      }
-      if (next) {
-        continue;
-      }
+  /**
+   * @brief Backward execution for `forward_pre`.
+   *
+   * @param input `pre_tranpose_` input.
+   * @param output `pre_tranpose_` output.
+   * @param propagate_down Flag whether or not to perform backward propagation.
+   * @param accum Flag whether or not to accumulate grad.
+   */
+  void convert_to_channel_first_backward(Variable *input, Variable *output,
+                                         const bool propagate_down,
+                                         const bool accum);
 
-      pre_transpose_shape.push_back(i);
-    }
-
-    pre_transpose_ = create_Transpose(ctx, pre_transpose_shape);
-    pre_transpose_->setup({input_pre}, {output_pre});
-
-    // Post-transpose
-    std::vector<int> post_transpose_shape(ndim, -1);
-
-    for (int i = 0; i < batch_axis.size(); i++) {
-      post_transpose_shape[batch_axis[i]] = i;
-    }
-
-    post_transpose_shape[channel_axis] = batch_axis.size();
-
-    int spacial_axis = batch_axis.size() + 1;
-    for (int i = 0; i < ndim; i++) {
-      if (post_transpose_shape[i] == -1) {
-        post_transpose_shape[i] = spacial_axis;
-        spacial_axis++;
-      }
-    }
-
-    input_post->reshape(output_pre->shape(), true);
-    post_transpose_ = create_Transpose(ctx, post_transpose_shape);
-    post_transpose_->setup({input_post}, {output_post});
-  }
-  void forward_pre(Variable *input, Variable *output) {
-    pre_transpose_->forward({input}, {output});
-  }
-  void forward_post(Variable *input, Variable *output) {
-    post_transpose_->forward({input}, {output});
-  }
-
-  void backward_pre(Variable *input, Variable *output,
-                    const bool propagate_down, const bool accum) {
-    pre_transpose_->backward({input}, {output}, {propagate_down}, {accum});
-  }
-  void backward_post(Variable *input, Variable *output,
-                     const bool propagate_down, const bool accum) {
-    post_transpose_->backward({input}, {output}, {propagate_down}, {accum});
-  }
+  /**
+   * @brief Backward execution for `forward_post`.
+   *
+   * @param input `post_tranpose_` input.
+   * @param output `post_tranpose_` output.
+   * @param propagate_down Flag whether or not to perform backward propagation.
+   * @param accum Flag whether or not to accumulate grad.
+   */
+  void convert_from_channel_first_backward(Variable *input, Variable *output,
+                                           const bool propagate_down,
+                                           const bool accum);
 };
+
+using ChannelFirstAdaptorPtr = std::shared_ptr<ChannelFirstAdaptor>;
 }
 
 #endif
