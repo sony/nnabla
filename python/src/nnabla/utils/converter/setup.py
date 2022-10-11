d1f8f7738159698bd2cff2c2e9ed9f5bbd9d0d09
woody.li <woody.li@sony.com>
2022-10-11 09:45:56 +0800

    upgrade onnx to 1.10.0

--- python/src/nnabla/utils/converter/setup.py
+++ python/src/nnabla/utils/converter/setup.py
@@ -42,7 +42,7 @@ if __name__ == '__main__':
         'onnx_tf',
         'tf2onnx==1.7.2',
         'tensorflow-addons',
-        'onnx==1.9.0',
+        'onnx==1.10.0',
         'tflite2onnx',
         'flatbuffers'
     ]
