65c55f1ba672d55f13973ec763e2df44e1b82a95
nnabla developer <nnabla@example.com>
2023-03-10 15:19:09 +0900

    add implementation of create_Callback and delete NBLA_API specifier of Callback::allowed_array_classes

--- src/nbla/function/nontemplate/callback.cpp
+++ src/nbla/function/nontemplate/callback.cpp
@@ -33,4 +33,13 @@ void Callback::backward_impl(const Variables &inputs, const Variables &outputs,
 vector<string> Callback::allowed_array_classes() {
   return SingletonManager::get<Cpu>()->array_classes();
 }
+
+NBLA_API shared_ptr<Function> create_Callback(
+    const Context &ctx, void *obj, int min_outputs,
+    Callback::setup_callback_type s, Callback::forward_callback_type f,
+    Callback::backward_callback_type b, Callback::cleanup_callback_type c,
+    Callback::grad_depends_output_data_callback_type go,
+    Callback::grad_depends_input_data_callback_type gi) {
+  return make_shared<Callback>(ctx, obj, min_outputs, s, f, b, c, go, gi);
+}
 }
